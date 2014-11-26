82cae5281b6713595cd33d2c3fc6af1d8f870609
Alexandr Maha <magasaniya@hotmail.com>
2014-11-26 23:37:39 +0200

    Task 40:[BL] Create Domain to DAL mapper.

---
+++ VolleyManagement.Dal.MsSql/Mappers/DomainToDal.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DomainToDal.cs" company="SoftServe">
+//   Copyright (c) SoftServe. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines IRepository contract.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SoftServe.VolleyManagement.Dal.MsSql.Mappers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Domain = VolleyManagement.Domain.Tournaments;
+    using Dal = VolleyManagement.Dal.MsSql;
+   
+    public static class DomainToDal
+    {
+        public static Dal.Tournament GetTourament(Domain.Tournament _tournament)
+        {
+            Dal.Tournament tournament = new Dal.Tournament();     
+            tournament.Id = to
+            return tournament;
+        }
+    }
+}

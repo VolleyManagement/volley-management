31650952be4135b7eb8dd10ee2a13aea6a7b0e7c
Maria <macosha@ukr.net>
2014-11-29 12:52:09 +0200

    [BL] Create contract for tournament service

--- VolleyManagement.Contracts/ITournamentService.cs
+++ VolleyManagement.Contracts/ITournamentService.cs
@@ -1,11 +1,28 @@
-﻿namespace SoftServe.VolleyManagement.Contracts
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ITournamentService.cs" company="SoftServe">
+//   Copyright (c) SoftServe. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines interface for TournamentService.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace SoftServe.VolleyManagement.Contracts
 {
-    using SoftServe.VolleyManagement.Domain.Tournaments;
     using System.Collections.Generic;
     using System.Linq;
+    using SoftServe.VolleyManagement.Domain.Tournaments;
 
+    /// <summary> 
+    /// Interface for TournamentService
+    /// </summary> 
     public interface ITournamentService
     {
+        /// <summary>
+        /// Gets list of all tournaments
+        /// </summary>
+        /// <returns>
+        /// Return list of all tournaments.
+        /// </returns>
         IQueryable<Tournament> GetAllTournaments();
     }
 }

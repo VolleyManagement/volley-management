e48de0606529a26ab997cfde855adbe1add16a90
Alex Lapin <ktif.al@gmail.com>
2014-12-01 12:41:00 +0200

    StyleCop corrections

--- VolleyManagement.Contracts/ITournamentService.cs
+++ VolleyManagement.Contracts/ITournamentService.cs
@@ -1,11 +1,26 @@
-﻿namespace VolleyManagement.Contracts
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ITournamentService.cs" company="SoftServe">
+//   Copyright (c) SoftServe. All rights reserved.
+// </copyright>
+// <summary>
+//   Defines interface for TournamentService.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace VolleyManagement.Contracts
 {
     using System.Linq;
+    using Domain.Tournaments;
 
-    using VolleyManagement.Domain.Tournaments;
-
+    /// <summary>
+    /// Interface for TournamentService
+    /// </summary>
     public interface ITournamentService
     {
+        /// <summary>
+        /// Gets list of all tournaments
+        /// </summary>
+        /// <returns>Return list of all tournaments.</returns>
         IQueryable<Tournament> GetAllTournaments();
     }
 }
--- VolleyManagement.Dal.MsSql/Tournament.cs
+++ VolleyManagement.Dal.MsSql/Tournament.cs
@@ -5,31 +5,49 @@ namespace VolleyManagement.Dal.MsSql
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
-    
+
     /// <summary>
     /// DAL tournament model
     /// </summary>
     [Table("Tournament")]
     public partial class Tournament
     {
+        /// <summary>
+        /// tournament id
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// tournament name
+        /// </summary>
         [Required]
         [StringLength(80)]
         public string Name { get; set; }
 
+        /// <summary>
+        /// tournament scheme
+        /// </summary>
         [Required]
         [StringLength(40)]
         public string Scheme { get; set; }
 
+        /// <summary>
+        /// tournament season
+        /// </summary>
         [Required]
         [StringLength(40)]
         public string Season { get; set; }
 
+        /// <summary>
+        /// tournament description
+        /// </summary>
         [Required]
         [StringLength(1024)]
         public string Description { get; set; }
 
+        /// <summary>
+        /// regulations of the tournament
+        /// </summary>
         [StringLength(1024)]
         public string LinkToReglament { get; set; }
     }

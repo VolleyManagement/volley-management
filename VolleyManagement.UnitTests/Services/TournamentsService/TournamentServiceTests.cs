585cba3d66d775828692a0ce30c00c2ffe8be858
Sergii Diachenko <sdiachenko@outlook.com>
2014-12-07 22:50:38 +0200

    Refactored Unit tests

---
+++ VolleyManagement.UnitTests/Services/TournamentsService/TournamentServiceTests.cs
@@ -0,0 +1,84 @@
+﻿namespace VolleyManagement.UnitTests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    using Ninject;
+
+    using VolleyManagement.Dal.Contracts;
+    using VolleyManagement.Domain.Tournaments;
+    using VolleyManagement.Services;
+
+    /// <summary>
+    /// Tests for TournamentService class.
+    /// </summary>
+    [TestClass]
+    public class TournamentServiceTests
+    {
+        /// <summary>
+        /// Test Fixture
+        /// </summary>
+        private readonly TournamentsServiceTestFixture _testFixture = new TournamentsServiceTestFixture();
+
+        /// <summary>
+        /// Tournaments Repo Mock
+        /// </summary>
+        private readonly Mock<ITournamentRepository> _tournamentRepositoryMock = new Mock<ITournamentRepository>();
+
+        /// <summary>
+        /// IoC for tests
+        /// </summary>
+        private IKernel _kernel;
+
+        /// <summary>
+        /// Initializes test data
+        /// </summary>
+        [TestInitialize]
+        public void TestInit()
+        {
+            this._kernel = new StandardKernel();
+            this._kernel.Bind<ITournamentRepository>()
+                   .ToConstant(this._tournamentRepositoryMock.Object);
+        }
+
+        /// <summary>
+        /// Test for GetAll() method. The method should return existing tournaments
+        /// (order is important).
+        /// </summary>
+        [TestMethod]
+        public void GetAll_TournamentsExist_TournamentsReturned()
+        {
+            // Arrange
+            var testData = this._testFixture.TestTournaments()
+                                       .Build();
+            this.MockTournaments(testData);
+            var tournamentService = this._kernel.Get<TournamentService>();
+
+            // sut - stands for System Under Test
+            var sut = new TournamentsServiceTestFixture()
+                                            .TestTournaments()
+                                            .Build();
+
+            // Act
+            var actual = tournamentService.GetAll()
+                                          .OrderBy(t => t.Id)
+                                          .ToList();
+
+            // Assert
+            Assert.IsTrue(sut.SequenceEqual(actual, new TournamentComparer()));
+        }
+
+        /// <summary>
+        /// Mocks test data
+        /// </summary>
+        /// <param name="testData">Data to mock</param>
+        private void MockTournaments(IEnumerable<Tournament> testData)
+        {
+            this._tournamentRepositoryMock.Setup(tr => tr.FindAll()).Returns(testData.AsQueryable());
+        }
+    }
+}

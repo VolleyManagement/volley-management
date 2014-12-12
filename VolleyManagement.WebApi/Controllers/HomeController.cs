2c69e32c2e4d886f642883ba9c2a487ffb8372b1
Oleh Vovkodav <ovovkodav@gmail.com>
2014-12-12 12:01:34 +0200

    Task 58:[BL] WebAPI routing for UI team

---
+++ VolleyManagement.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿namespace VolleyManagement.WebApi.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Defines HomeController which responsible for returning Index for all routes
+    /// except routes for OData endpoint
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Action returns Index view
+        /// </summary>
+        /// <returns>Index view</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

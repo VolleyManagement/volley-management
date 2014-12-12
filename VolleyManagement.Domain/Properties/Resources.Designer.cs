80f15b932017f2d5eed3e350b18697b2d604a081
Viktoriya Ryndina <ryndina.vika@gmail.com>
2014-12-12 14:27:07 +0200

    Validation for tournament domain model

---
+++ VolleyManagement.Domain/Properties/Resources.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace VolleyManagement.Domain.Properties {
+    using System;
+    
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    internal class Resources {
+        
+        private static global::System.Resources.ResourceManager resourceMan;
+        
+        private static global::System.Globalization.CultureInfo resourceCulture;
+        
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources() {
+        }
+
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VolleyManagement.Domain.Properties.Resources", typeof(Resources).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
+                return resourceCulture;
+            }
+            set {
+                resourceCulture = value;
+            }
+        }
+        
+        internal static string ValidationResultDescription {
+            get {
+                return ResourceManager.GetString("ValidationResultDescription", resourceCulture);
+            }
+        }
+
+        internal static string ValidationResultName {
+            get {
+                return ResourceManager.GetString("ValidationResultName", resourceCulture);
+            }
+        }
+        
+        internal static string ValidationResultRegLink {
+            get {
+                return ResourceManager.GetString("ValidationResultRegLink", resourceCulture);
+            }
+        }
+        
+        internal static string ValidationResultScheme {
+            get {
+                return ResourceManager.GetString("ValidationResultScheme", resourceCulture);
+            }
+        }
+        
+        internal static string ValidationResultSeason {
+            get {
+                return ResourceManager.GetString("ValidationResultSeason", resourceCulture);
+            }
+        }
+    }
+}
--- VolleyManagement.Domain/Tournaments/Tournament.cs
+++ VolleyManagement.Domain/Tournaments/Tournament.cs
@@ -1,11 +1,15 @@
 ﻿namespace VolleyManagement.Domain.Tournaments
 {
+    using System;
+    using System.Collections.Generic;
     using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using VolleyManagement.Domain.Properties;
 
     /// <summary>
     /// Tournament domain class.
     /// </summary>
-    public class Tournament
+    public class Tournament : IValidatableObject
     {
         /// <summary>
         /// Gets or sets a value indicating where Id.
@@ -42,5 +46,41 @@
         /// </summary>
         /// <value>regulations of tournament.</value>
         public string RegulationsLink { get; set; }
+
+        /// <summary>
+        /// Validates the model properties
+        /// </summary>
+        /// <param name="validationContext">Context of Validation</param>
+        /// <returns>List of validation errors</returns>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (string.IsNullOrEmpty(Name) && Name.Length >= 80)
+            {
+                errors.Add(new ValidationResult(Resources.ValidationResultName));
+            }
+
+            if (Description.Length >= 1024)
+            {
+                errors.Add(new ValidationResult(Resources.ValidationResultDescription));
+            }
+
+            if (string.IsNullOrEmpty(Season) && Season.Length >= 9)
+            {
+                errors.Add(new ValidationResult(Resources.ValidationResultSeason));
+            }
+
+            if (Enum.IsDefined(typeof(TournamentSchemeEnum), Scheme))
+            {
+                errors.Add(new ValidationResult(Resources.ValidationResultScheme));
+            }
+
+            if (RegulationsLink.Length >= 1024)
+            {
+                errors.Add(new ValidationResult(Resources.ValidationResultRegLink));
+            }
+            return errors;
+        }
     }
 }

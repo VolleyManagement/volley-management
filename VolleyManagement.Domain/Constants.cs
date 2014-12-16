ad31a311c81efa3837602d9116e337b81f3940ba
Viktoriya Ryndina <ryndina.vika@gmail.com>
2014-12-16 23:24:06 +0200

    Update Domain model - Edit constant names

--- VolleyManagement.Domain/Constants.cs
+++ VolleyManagement.Domain/Constants.cs
@@ -10,21 +10,21 @@
         /// <summary>
         /// constant defined the length of the name field
         /// </summary>
-        public const int nameLength = 60;
+        public const int MaxNameLength = 60;
 
         /// <summary>
         /// constant defined the length of the description field
         /// </summary>
-        public const int descriptionLength = 300;
+        public const int MaxDescriptionLength = 300;
 
         /// <summary>
         /// constant defined the length of the season field
         /// </summary>
-        public const int seasonLength = 9;
+        public const int MaxSeasonLength = 9;
 
         /// <summary>
-        /// constant defined the length of the regilationsLink field
+        /// constant defined the length of the regulation link field
         /// </summary>
-        public const int regLinkLength = 255;
+        public const int MaxRegLinkLength = 255;
     }
 }

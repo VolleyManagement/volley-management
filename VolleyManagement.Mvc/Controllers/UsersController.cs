4480dd9d432fb62db69e9825411a62ee84b3b987
Viktoriya Ryndina <ryndina.vika@gmail.com>
2014-12-23 21:01:54 +0200

    Update MVC controller. Add UsersController file

---
+++ VolleyManagement.Mvc/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿namespace VolleyManagement.Mvc.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using VolleyManagement.Contracts;
+
+    /// <summary>
+    /// Defines UsersController
+    /// </summary>
+    public class UsersController : Controller
+    {
+        /// <summary>
+        /// Holds UserService instance
+        /// </summary>
+        private readonly IUserService _userService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UsersController"/> class
+        /// </summary>
+        /// <param name="userService">The user service</param>
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// Gets all users with UserService
+        /// </summary>
+        /// <returns>View with collection of users</returns>
+        public ActionResult Index()
+        {
+            try
+            {
+                var users = _userService.GetAll().ToList();
+                return View(users);
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}

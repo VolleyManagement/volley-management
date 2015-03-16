50f0b9deed8ef96f0a5c5ed41c6048cbfc87d694
stripedl <stripedl@mail.ru>
2015-03-16 21:30:50 +0200

    Get and Post test fixing

--- VolleyManagement.UnitTests/WebApi/Controllers/UserControllerTests.cs
+++ VolleyManagement.UnitTests/WebApi/Controllers/UserControllerTests.cs
@@ -6,6 +6,8 @@
     using System.Linq;
     using System.Net;
     using System.Net.Http;
+    using System.Web.Http.Results;
+    using System.Web.Http.OData.Results;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
     using Ninject;
@@ -23,6 +25,16 @@
     [TestClass]
     public class UserControllerTests
     {
+        /// <summary>
+        /// An unassigned user identifier.
+        /// </summary>
+        private const int UNASSIGNED_USER_ID = 0;
+
+        /// <summary>
+        /// An expected test user identifier.
+        /// </summary>
+        private const int EXPECTED_USER_ID = 10;
+
         /// <summary>
         /// Test Fixture
         /// </summary>
@@ -47,7 +59,54 @@
             _kernel = new StandardKernel();
             _kernel.Bind<IUserService>().ToConstant(_userServiceMock.Object);
         }
-        
+
+        /// <summary>
+        /// Test Post method. Basic story.
+        /// </summary>
+        [TestMethod]
+        public void Post_ValidViewModel_UserCreated()
+        {
+            // Arrange
+            var controller = _kernel.Get<UsersController>();
+            var expected = new UserViewModelBuilder().Build();
+            expected.Id = EXPECTED_USER_ID;
+            _userServiceMock.Setup(us => us.Create(It.IsAny<User>()))
+                .Callback((User u) => { u.Id = EXPECTED_USER_ID; });
+            var input = new UserViewModelBuilder()
+                .WithId(UNASSIGNED_USER_ID)
+                .Build();
+
+
+            // Act
+            var createdResult = controller.Post(input) as
+                CreatedODataResult<UserViewModel>;
+
+            // Assert
+            Assert.IsNotNull(createdResult);
+            var actual = createdResult.Entity;
+            _userServiceMock.Verify(us => us.Create(It.IsAny<User>()), Times.Once);
+            AssertExtensions.AreEqual<UserViewModel>(expected, actual, new UserViewModelComparer());
+        }
+
+        /// <summary>
+        /// Test for Get() method. Method should return existing users.
+        /// </summary>
+        [TestMethod]
+        public void Get_UsersExist_UsersReturned()
+        {
+            // Arrange
+            var testData = _testFixture.TestUsers().Build();
+            MockUsers(testData);
+            var controller = _kernel.Get<UsersController>();
+            var expected = GetExpectedUsersList();
+
+            // Act
+            var actual = controller.GetUsers().ToList();
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual, new UserViewModelComparer());
+        }
+
         /// <summary>
         /// Gets generic T model from response content
         /// </summary>
@@ -76,6 +135,41 @@
         private void MockUsers(IEnumerable<User> testData)
         {
             _userServiceMock.Setup(u => u.Get()).Returns(testData.AsQueryable());
+        }
+
+        /// <summary>
+        /// Creates a test UserViewModel collection with the same data as
+        /// in the UserServiceTestFixture.
+        /// </summary>
+        /// <returns>A list of UserViewModel with test data.</returns>
+        private List<UserViewModel> GetExpectedUsersList()
+        {
+            var userModels = new List<UserViewModel>();
+            userModels.Add(new UserViewModelBuilder()
+                .WithId(1)
+                .WithUserName("testA")
+                .WithFullName("Test Name A")
+                .WithEmail("test1@gmail.com")
+                .WithPassword(string.Empty)
+                .WithCellPhone("0500000001")
+                .Build());
+            userModels.Add(new UserViewModelBuilder()
+                .WithId(2)
+                .WithUserName("testB")
+                .WithFullName("Test Name B")
+                .WithEmail("test2@gmail.com")
+                .WithPassword(string.Empty)
+                .WithCellPhone("0500000002")
+                .Build());
+            userModels.Add(new UserViewModelBuilder()
+                .WithId(3)
+                .WithUserName("testC")
+                .WithFullName("Test Name C")
+                .WithEmail("test3@gmail.com")
+                .WithPassword(string.Empty)
+                .WithCellPhone("0500000003")
+                .Build());
+            return userModels;
         }
     }
 }

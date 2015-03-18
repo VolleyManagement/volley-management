2ac34223944f27555063738ea9252055f5cdb514
d-shapoval <tesaurus@ua.fm>
2015-03-18 22:22:39 +0200

    Divide test Post_ValidViewModel_TournamentCreated to two methods: Post_ValidViewModel_TournamentCreated and Post_Valid_Id_TournamentCreated. Fix it according to current architecture

--- VolleyManagement.UnitTests/WebApi/Controllers/TournamentControllerTests.cs
+++ VolleyManagement.UnitTests/WebApi/Controllers/TournamentControllerTests.cs
@@ -15,6 +15,7 @@
     using VolleyManagement.UI.Areas.WebApi.ApiControllers;
     using VolleyManagement.UI.Areas.WebApi.ViewModels.Tournaments;
     using VolleyManagement.UnitTests.WebApi.ViewModels;
+    using System.Web.Http.OData.Results;
 
     /// <summary>
     /// Tests for TournamentController class.
@@ -126,23 +127,47 @@
         /// Test Post method. Basic story.
         /// </summary>
         [TestMethod]
-        [Ignore]// BUG: FIX ASAP
         public void Post_ValidViewModel_TournamentCreated()
         {
             // Arrange
-            //var controller = _kernel.Get<TournamentsController>();
-            //TestExtensions.SetControllerRequest(controller);
-            //var expected = new TournamentViewModelBuilder().Build();
+            var controller = _kernel.Get<TournamentsController>();
+            var expected = new TournamentViewModelBuilder().WithId(10).Build();
+            _tournamentServiceMock.Setup(ts => ts.Create(It.IsAny<Tournament>()))
+                .Callback((Tournament t) => { t.Id = 10; });
 
-            //// Act
-            //var response = controller.Post(expected);
-            //var actual = TestExtensions.GetModelFromResponse<TournamentViewModel>(response);
+            // Act
+            var input = new TournamentViewModelBuilder().WithId(0).Build();
+            var response = controller.Post(input);
+            var actual = ((CreatedODataResult<TournamentViewModel>)response).Entity;
 
-            //// Assert
-            //_tournamentServiceMock.Verify(ts => ts.Create(It.IsAny<Tournament>()), Times.Once());
-            //Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
-            //AssertExtensions.AreEqual<TournamentViewModel>(expected, actual, new TournamentViewModelComparer());
+            // Assert
+
+            _tournamentServiceMock.Verify(ts => ts.Create(It.IsAny<Tournament>()), Times.Once());
+            AssertExtensions.AreEqual<TournamentViewModel>(expected, actual, new TournamentViewModelComparer());
         }
+
+        /// <summary>
+        /// Test Post method. Basic story.
+        /// </summary>
+        [TestMethod]
+        public void Post_Valid_Id_TournamentCreated()
+        {
+            // Arrange
+            var controller = _kernel.Get<TournamentsController>();
+            var expected = new TournamentViewModelBuilder().WithId(10).Build();
+            _tournamentServiceMock.Setup(ts => ts.Create(It.IsAny<Tournament>()))
+                .Callback((Tournament t) => { t.Id = 10; });
+
+            // Act
+            var input = new TournamentViewModelBuilder().WithId(0).Build();
+            var response = controller.Post(input);
+            var actual = ((CreatedODataResult<TournamentViewModel>)response).Entity;
+
+            // Assert
+            _tournamentServiceMock.Verify(ts => ts.Create(It.IsAny<Tournament>()), Times.Once());
+            Assert.AreEqual<int>(expected.Id, actual.Id);
+        }
+
 
         /// <summary>
         /// Test for Delete() method

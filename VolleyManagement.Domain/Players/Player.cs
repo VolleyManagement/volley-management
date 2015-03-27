c39b82c2bc07a22f2e03d65a4aea2491069aa897
stripedl <stripedl@mail.ru>
2015-03-27 20:51:00 +0200

    Add IPlayerService and. PlayerValidationException.

---
+++ VolleyManagement.Domain/Players/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace VolleyManagement.Domain.Players
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Player domain class.
+    /// </summary>
+    public class Player
+    {
+    }
+}

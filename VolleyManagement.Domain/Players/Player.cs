539b7c9da39f7daaecce4a5da404c2644b645a9c
stripedl <stripedl@mail.ru>
2015-03-27 21:58:16 +0200

    Add Player and PlayerValidation classes.

--- VolleyManagement.Domain/Players/Player.cs
+++ VolleyManagement.Domain/Players/Player.cs
@@ -1,15 +1,133 @@
 ﻿namespace VolleyManagement.Domain.Players
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using VolleyManagement.Domain.Properties;
 
     /// <summary>
     /// Player domain class.
     /// </summary>
     public class Player
     {
+        private string _firstName;
+        private string _lastName;
+        private int _birthYear;
+        private int _height;
+        private int _weight;
+
+        /// <summary>
+        /// Gets or sets a value indicating where Id.
+        /// </summary>
+        /// <value>Id of player.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating where FirstName.
+        /// </summary>
+        /// <value>First name.</value>
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+
+            set
+            {
+                if (PlayerValidation.ValidateFirstName(value))
+                {
+                    throw new ArgumentException(Resources.ValidationPlayerFirstName);
+                }
+
+                _firstName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating where LastName.
+        /// </summary>
+        /// <value>Last name.</value>
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                if (PlayerValidation.ValidateLastName(value))
+                {
+                    throw new ArgumentException(Resources.ValidationPlayerLastName);
+                }
+
+                _lastName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating where BirthYear.
+        /// </summary>
+        /// <value>Birth year.</value>
+        public int BirthYear
+        {
+            get
+            {
+                return _birthYear;
+            }
+
+            set
+            {
+                if (PlayerValidation.ValidateBirthYear(value))
+                {
+                    throw new ArgumentException(Resources.ValidationPlayerBirthYear);
+                }
+
+                _birthYear = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating where Height.
+        /// </summary>
+        /// <value>The height.</value>
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+
+            set
+            {
+                if (PlayerValidation.ValidateHeight(value))
+                {
+                    throw new ArgumentException(Resources.ValidationPlayerHeight);
+                }
+
+                _height = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating where Weight.
+        /// </summary>
+        /// <value>The weight.</value>
+        public int Weight
+        {
+            get
+            {
+                return _weight;
+            }
+
+            set
+            {
+                if (PlayerValidation.ValidateWeight(value))
+                {
+                    throw new ArgumentException(Resources.ValidationPlayerWeight);
+                }
+
+                _weight = value;
+            }
+        }
     }
 }

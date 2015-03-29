3c1119d18e252af8c84793b3c7803b03fc5fbf4c
ryndin <ryn.dmi3y@gmail.com>
2015-03-29 11:06:55 +0300

    [BL] MVC layer
    Add empy player mvc controller

---
+++ VolleyManagement.UI/Areas/Mvc/Controllers/PlayersController.cs
@@ -0,0 +1,14 @@
+﻿
+namespace VolleyManagement.UI.Areas.Mvc.Controllers
+{
+    using System.Web;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Defines player controller
+    /// </summary>
+    public class PlayersController : Controller
+    {
+
+    }
+}

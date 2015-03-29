91d5e3ea06eb1a2dabbd0a3b5f69b8c4a67f2c29
ryndin <ryn.dmi3y@gmail.com>
2015-03-29 12:05:58 +0300

    [BL] MVC layer
    Add service classes using for testing players View model

--- VolleyManagement.UI/Areas/Mvc/ViewModels/Players/PlayerViewModel.cs
+++ VolleyManagement.UI/Areas/Mvc/ViewModels/Players/PlayerViewModel.cs
@@ -2,7 +2,10 @@
 namespace VolleyManagement.UI.Areas.Mvc.ViewModels.Players
 {
     using System.ComponentModel.DataAnnotations;
+
+    using VolleyManagement.Domain.Players;
     using VolleyManagement.UI.App_GlobalResources;
+    using playerConst = VolleyManagement.Domain.Constants.Player;
 
     /// <summary>
     /// Represents player view model
@@ -18,18 +21,24 @@ namespace VolleyManagement.UI.Areas.Mvc.ViewModels.Players
         /// Gets or sets the player first name
         /// </summary>
         [Display(Name = "PlayerFirstName", ResourceType = typeof(ViewModelResources))]
-        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ViewModelResources))]
-        [StringLength(60, ErrorMessageResourceName = "MaxLengthErrorMessage", ErrorMessageResourceType = typeof(ViewModelResources))]
-        [RegularExpression(@"\p{L}+$", ErrorMessageResourceName = "InvalidEntriesError", ErrorMessageResourceType = typeof(ViewModelResources))]
+        [Required(ErrorMessageResourceName = "FieldRequired"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
+        [StringLength(playerConst.MAX_FIRST_NAME_LENGTH, ErrorMessageResourceName = "MaxLengthErrorMessage"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
+        [RegularExpression(@"\p{L}+$", ErrorMessageResourceName = "InvalidEntriesError"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
         public string FirstName { get; set; }
 
         /// <summary>
         /// Gets or sets the player last name
         /// </summary>
         [Display(Name = "PlayerLastName", ResourceType = typeof(ViewModelResources))]
-        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(ViewModelResources))]
-        [StringLength(60, ErrorMessageResourceName = "MaxLengthErrorMessage", ErrorMessageResourceType = typeof(ViewModelResources))]
-        [RegularExpression(@"\p{L}+$", ErrorMessageResourceName = "InvalidEntriesError", ErrorMessageResourceType = typeof(ViewModelResources))]
+        [Required(ErrorMessageResourceName = "FieldRequired"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
+        [StringLength(playerConst.MAX_LAST_NAME_LENGTH, ErrorMessageResourceName = "MaxLengthErrorMessage"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
+        [RegularExpression(@"\p{L}+$", ErrorMessageResourceName = "InvalidEntriesError"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
         public string LastName { get; set; }
 
         /// <summary>
@@ -41,22 +50,24 @@ namespace VolleyManagement.UI.Areas.Mvc.ViewModels.Players
         /// <summary>
         /// Gets or sets the player birth year
         /// </summary>
-        [Range(1900, 2100, ErrorMessageResourceName = "FieldRange", ErrorMessageResourceType = typeof(ViewModelResources))]
+        [Range(playerConst.MIN_BIRTH_YEAR, playerConst.MAX_BIRTH_YEAR
+            , ErrorMessageResourceName = "FieldRange", ErrorMessageResourceType = typeof(ViewModelResources))]
         public int BirthYear { get; set; }
 
         /// <summary>
         /// Gets or sets the player height
         /// </summary>
-        [Range(10, 250, ErrorMessageResourceName = "FieldRange", ErrorMessageResourceType = typeof(ViewModelResources))]
+        [Range(playerConst.MIN_HEIGHT, playerConst.MAX_HEIGHT
+            , ErrorMessageResourceName = "FieldRange", ErrorMessageResourceType = typeof(ViewModelResources))]
         public int Height { get; set; }
 
         /// <summary>
         /// Gets or sets the player weight
         /// </summary>
-        [Range(10, 250, ErrorMessageResourceName = "FieldRange", ErrorMessageResourceType = typeof(ViewModelResources))]
+        [Range(playerConst.MIN_WEIGHT, playerConst.MAX_WEIGHT
+            , ErrorMessageResourceName = "FieldRange", ErrorMessageResourceType = typeof(ViewModelResources))]
         public int Weight { get; set; }
 
-        /*
         #region Factory Methods
 
         /// <summary>
@@ -68,13 +79,13 @@ namespace VolleyManagement.UI.Areas.Mvc.ViewModels.Players
         {
             var playerViewModel = new PlayerViewModel
             {
-                Id = ,
-                FirstName = ,
-                LastName = ,
-                Photo = ,
-                BirthYear = ,
-                Height = ,
-                Weight = 
+                Id = player.Id,
+                FirstName = player.FirstName,
+                LastName = player.LastName,
+                // TO DO: Photo = player.,
+                BirthYear = player.BirthYear,
+                Height = player.Height,
+                Weight = player.Weight
             };
 
             return playerViewModel;
@@ -88,10 +99,16 @@ namespace VolleyManagement.UI.Areas.Mvc.ViewModels.Players
         {
             return new Player
             {
+                Id = this.Id,
+                FirstName = this.FirstName,
+                LastName = this.LastName,
+                // TO DO: Photo
+                BirthYear = this.BirthYear,
+                Height = this.Height,
+                Weight = this.Weight
             };
         }
         #endregion
-         */
         
     }
 }
---
+++ VolleyManagement.UnitTests/Mvc/ViewModels/PlayerViewModelComparer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace VolleyManagement.UnitTests.Mvc.ViewModels
+{
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    using VolleyManagement.UI.Areas.Mvc.ViewModels.Players;
+
+    /// <summary>
+    /// Comparer for player objects.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    internal class PlayerViewModelComparer : IComparer<PlayerViewModel>
+    {
+        /// <summary>
+        /// Compares two players objects.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>A signed integer that indicates the relative values of players.</returns>
+        public int Compare(PlayerViewModel x, PlayerViewModel y)
+        {
+            if (IsEqual(x, y))
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        /// <summary>
+        /// Finds out whether two player objects have the same properties.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>True if given players have the same properties.</returns>
+        private bool IsEqual(PlayerViewModel x, PlayerViewModel y)
+        {
+            return x.Id == y.Id &&
+                x.FirstName == y.FirstName &&
+                x.LastName == y.LastName &&
+                x.Photo == y.Photo &&
+                x.BirthYear == y.BirthYear &&
+                x.Height == y.Height &&
+                x.Weight == y.Weight;
+        }
+    }
+}

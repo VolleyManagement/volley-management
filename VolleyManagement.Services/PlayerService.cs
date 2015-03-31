012a6451d3ae84128f677fb3070acbfcd0e31a2f
stripedl <stripedl@mail.ru>
2015-03-31 13:52:05 +0300

    Add blank PlayerService class

---
+++ VolleyManagement.Services/PlayerService.cs
@@ -0,0 +1,17 @@
+﻿namespace VolleyManagement.Services
+{
+    using System;
+    using System.Linq;
+    using System.Web.Helpers;
+    using VolleyManagement.Contracts;
+    using VolleyManagement.Dal.Contracts;
+    using VolleyManagement.Domain.Properties;
+    using VolleyManagement.Domain.Players;
+
+    /// <summary>
+    /// Defines PlayerService
+    /// </summary>
+    public class PlayerService
+    {
+    }
+}

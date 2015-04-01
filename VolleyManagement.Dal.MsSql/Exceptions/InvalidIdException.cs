d3cac7484261ffce72473ad81b924da9abf71410
d-shapoval <tesaurus@ua.fm>
2015-04-01 23:37:29 +0300

    Refactor according to comments

---
+++ VolleyManagement.Dal.MsSql/Exceptions/InvalidIdException.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace VolleyManagement.Dal.MsSql.Exceptions
+{
+    using System;
+
+    class InvalidIdException : ArgumentException
+    {
+        public InvalidIdException():base("Specified id is not exist")
+        { }
+    }
+}

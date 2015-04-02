ff789b0fcb5a2b4de6ece81dee0fc949366c40fd
Misha <25lion52@gmail.com>
2015-04-02 14:03:03 +0300

    Delete excess code

--- VolleyManagement.UI/Areas/Mvc/ViewModels/Players/PlayersListViewModel.cs
+++ VolleyManagement.UI/Areas/Mvc/ViewModels/Players/PlayersListViewModel.cs
@@ -27,8 +27,7 @@
 
             this.List = new List<PlayerViewModel>(source.Skip((this.PageNumber - 1) * Size).Take(Size)
                 .ToList()
-                .Select(p => PlayerViewModel.Map(p))
-                .AsQueryable());
+                .Select(p => PlayerViewModel.Map(p)));
         }
 
         /// <summary>

403e62ca58703fe6205098b774820f7031e2afa6
d-shapoval <tesaurus@ua.fm>
2015-04-16 13:38:23 +0300

    Update contracts

---
+++ VolleyManagement.Contracts/ITeamService.cs
@@ -0,0 +1,29 @@
+﻿namespace VolleyManagement.Contracts
+{
+    using System.Linq;
+    using Domain.Teams;
+
+    /// <summary>
+    /// Interface for TeamService.
+    /// </summary>
+    public interface ITeamService
+    {
+        /// <summary>
+        /// Gets list of all teams.
+        /// </summary>
+        /// <returns>Return list of all teams.</returns>
+        IQueryable<Team> Get();
+
+        /// <summary>
+        /// Create new team.
+        /// </summary>
+        /// <param name="team">New team.</param>
+        void Create(Team team);
+
+        /// <summary>
+        /// Delete team by id.
+        /// </summary>
+        /// <param name="id">Team id.</param>
+        void Delete(int id);
+    }
+}

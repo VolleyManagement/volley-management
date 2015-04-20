8da6b0dd409cd0126f05021a94de2d61aa49085f
stripedl <stripedl@mail.ru>
2015-04-20 18:32:37 +0300

    Add TeamViewModel

---
+++ VolleyManagement.UI/Areas/WebAPI/ApiControllers/TeamsController.cs
@@ -0,0 +1,52 @@
+﻿namespace VolleyManagement.UI.Areas.WebApi.ApiControllers
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Net;
+    using System.Web.Http;
+    using System.Web.OData;
+
+    using VolleyManagement.Contracts;
+    using VolleyManagement.Contracts.Exceptions;
+    using VolleyManagement.UI.Areas.WebApi.ViewModels.Teams;
+
+    /// <summary>
+    /// The teams controller.
+    /// </summary>
+    public class TeamsController : ODataController
+    {
+        private readonly ITeamService _teamService;
+
+        private const string CONTROLLER_NAME = "teams";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TeamsController"/> class.
+        /// </summary>
+        /// <param name="teamService"> The team service. </param>
+        public TeamsController(ITeamService teamService)
+        {
+            _teamService = teamService;
+        }
+
+        /// <summary>
+        /// Creates Team
+        /// </summary>
+        /// <param name="team"> The  team as ViewModel. </param>
+        /// <returns> Has been saved successfully - Created OData result
+        /// unsuccessfully - Bad request </returns>
+        public IHttpActionResult Post(TeamViewModel team)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var teamToCreate = team.ToDomain();
+            _teamService.Create(teamToCreate);
+            team.Id = teamToCreate.Id;
+
+            return Created(team);
+        }
+    }
+}
---
+++ VolleyManagement.UI/Areas/WebAPI/ViewModels/Teams/TeamViewModel.cs
@@ -0,0 +1,86 @@
+﻿namespace VolleyManagement.UI.Areas.WebApi.ViewModels.Teams
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using VolleyManagement.Domain;
+    using VolleyManagement.Domain.Teams;
+    using VolleyManagement.UI.App_GlobalResources;
+
+    /// <summary>
+    /// Represents team view model
+    /// </summary>
+    public class TeamViewModel
+    {
+        /// <summary>
+        /// Gets or sets the team Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the captain Id.
+        /// </summary>
+        public int CaptainId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the team name.
+        /// </summary>
+        [Display(Name = "TeamName", ResourceType = typeof(ViewModelResources))]
+        [StringLength(Constants.Team.MAX_NAME_LENGTH, ErrorMessageResourceName = "MaxLengthErrorMessage"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the team coach.
+        /// </summary>
+        [Display(Name = "TeamCoach", ResourceType = typeof(ViewModelResources))]
+        [StringLength(Constants.Team.MAX_COACH_NAME_LENGTH, ErrorMessageResourceName = "MaxLengthErrorMessage"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
+        public string Coach { get; set; }
+
+        /// <summary>
+        /// Gets or sets the team achievements.
+        /// </summary>
+        [Display(Name = "TeamAchievements", ResourceType = typeof(ViewModelResources))]
+        [StringLength(Constants.Team.MAX_ACHIEVEMENTS_LENGTH, ErrorMessageResourceName = "MaxLengthErrorMessage"
+            , ErrorMessageResourceType = typeof(ViewModelResources))]
+        public string Achievements { get; set; }
+
+        #region Factory Methods
+
+        /// <summary>
+        /// Maps domain entity to presentation
+        /// </summary>
+        /// <param name="team"> Domain object </param>
+        /// <returns> View model object </returns>
+        public static TeamViewModel Map(Team team)
+        {
+            var teamViewModel = new TeamViewModel
+            {
+                Id = team.Id,
+                Name = team.Name,
+                Coach = team.Coach,
+                Achievements = team.Achievements,
+                //CaptainId = team.CaptainId
+            };
+
+            return teamViewModel;
+        }
+
+        /// <summary>
+        /// Maps presentation entity to domain
+        /// </summary>
+        /// <returns> Domain object </returns>
+        public Team ToDomain()
+        {
+            return new Team
+            {
+                Id = this.Id,
+                Name = this.Name,
+                Coach = this.Coach,
+                Achievements = this.Achievements,
+                //CaptainId = this.CaptainId
+            };
+        }
+        #endregion
+    }
+}

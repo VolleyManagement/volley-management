abeeb036b035201096547770a622ba650b78bd3a
ryndin <ryn.dmi3y@gmail.com>
2015-04-20 18:39:09 +0300

    Menu

---
+++ VolleyManagement.UI/Areas/Mvc/Controllers/NavController.cs
@@ -0,0 +1,16 @@
+﻿namespace VolleyManagement.UI.Areas.Mvc.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class NavController : Controller
+    {
+        public string Menu()
+        {
+            return "Test message";
+        }
+    }
+}
---
+++ VolleyManagement.UI/Areas/Mvc/ViewModels/Menu/MenuItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VolleyManagement.UI.Areas.Mvc.ViewModels.Menu
+{
+    public class MenuItemViewModel
+    {
+        public string Name { get; set; }
+
+        public bool IsCurentItem { get; set; }
+    }
+}

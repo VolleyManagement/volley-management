903e549e55bad5ac8fb053eb2076e9deb9bcdd62
Misha <25lion52@gmail.com>
2015-04-21 20:54:14 +0300

    Added action delete and tests

---
+++ VolleyManagement.UI/Areas/WebAPI/ApiControllers/TeamsController.cs
@@ -0,0 +1,40 @@
+﻿namespace VolleyManagement.UI.Areas.WebApi.ApiControllers
+{
+    using System.Net;
+    using System.Web.Http;
+    using System.Web.OData;
+
+    using VolleyManagement.Contracts;
+    using VolleyManagement.Contracts.Exceptions;
+
+    public class TeamsController : ODataController
+    {
+        private readonly ITeamService _teamService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TeamsController"/> class.
+        /// </summary>
+        /// <param name="teamService"> The team service. </param>
+        public TeamsController(ITeamService teamService)
+        {
+            this._teamService = teamService;
+        }
+
+        /// <summary> Deletes team </summary>
+        /// <param name="id"> The id. </param>
+        /// <returns> The <see cref="IHttpActionResult"/>. </returns>
+        public IHttpActionResult Delete([FromODataUri] int id)
+        {
+            try
+            {
+                _teamService.Delete(id);
+            }
+            catch (MissingEntityException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}

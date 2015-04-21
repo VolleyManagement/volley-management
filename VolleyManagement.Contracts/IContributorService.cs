3a659fa4b927b065870d17dac03cefb5107d66ff
rudenko-den <deru35712>
2015-04-22 00:10:18 +0300

    Page "About" - everything work correctly
    All user story is done - DB, BL, UI(WebApi, Mvc)
    
    List of contributors you can get with requests:
    http://localhost:23801/Mvc/Contributors/Index
    http://localhost:23801/Odata/Contributors
    
    I didn`t create menu item on layout, cause I don`t know how it should looks

--- VolleyManagement.Contracts/IContributorService.cs
+++ VolleyManagement.Contracts/IContributorService.cs
@@ -13,5 +13,30 @@
         /// </summary>
         /// <returns>Return list of all contributors.</returns>
         IQueryable<Contributor> Get();
+
+        /// <summary>
+        /// Create new contributor.
+        /// </summary>
+        /// <param name="contributor">New contributor.</param>
+        void Create(Contributor contributor);
+
+        /// <summary>
+        /// Edit contributor profile.
+        /// </summary>
+        /// <param name="contributor">Updated contributor data.</param>
+        void Edit(Contributor contributor);
+
+        /// <summary>
+        /// Find contributor by id.
+        /// </summary>
+        /// <param name="id">contributor id.</param>
+        /// <returns>Found contributor.</returns>
+        Contributor Get(int id);
+
+        /// <summary>
+        /// Delete contributor by id.
+        /// </summary>
+        /// <param name="id">contributor id.</param>
+        void Delete(int id);
     }
 }
--- VolleyManagement.Services/Infrastructure/NinjectServiceBindModule.cs
+++ VolleyManagement.Services/Infrastructure/NinjectServiceBindModule.cs
@@ -35,7 +35,8 @@
                               {
                                   Bind<ITournamentService>().To<TournamentService>().BindingConfiguration,
                                   Bind<IUserService>().To<UserService>().BindingConfiguration,
-                                  Bind<IPlayerService>().To<PlayerService>().BindingConfiguration
+                                  Bind<IPlayerService>().To<PlayerService>().BindingConfiguration,
+                                  Bind<IContributorService>().To<ContributorService>().BindingConfiguration
                               };
             if (_scopeCallback != null)
             {

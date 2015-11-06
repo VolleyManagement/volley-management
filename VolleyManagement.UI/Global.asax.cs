65f0985e776fe4feb70251d5b610bb7ac1636a9c
Pavel Novichikhin <novichikhinpavel@gmail.com>
2015-11-06 09:35:58 +0200

    Fixing issues with EntityFramework

--- VolleyManagement.UI/Global.asax.cs
+++ VolleyManagement.UI/Global.asax.cs
@@ -26,5 +26,18 @@
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        /// <summary>
+        /// Fix problem Entity framework
+        /// </summary>
+        public void FixEfProviderServicesProblem()
+        {
+            ////The Entity Framework provider type 'System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer'
+            ////for the 'System.Data.SqlClient' ADO.NET provider could not be loaded. 
+            ////Make sure the provider assembly is available to the running application. 
+            ////See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
+
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
     }
 }

f92b6f242f41906240aeaeca8191157540ad7b87
unknown <dshapo@DP010.softservecom.com>
2015-11-09 15:14:43 +0200

    Add VMscripts object

---
+++ VolleyManagement.UI/Scripts/VMScripts.js
@@ -0,0 +1,20 @@
+﻿var VM = (function () {
+
+    return {
+
+        addNamespace: function (namespaceName) {
+
+            var nsNames = namespaceName.split('.'),
+                currentNs = VM,
+                currentName;
+
+            for (var i = 0; i < nsNames.length; i++) {
+                currentName = nsNames[i];
+                if (!currentNs[currentName]) {
+                    currentNs[currentName] = {};
+                }
+                currentNs = currentNs[currentName];
+            }
+        }
+    };
+})();

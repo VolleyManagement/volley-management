5cd3cc4ee651bc48fefa6fd8c220e6c647760e8c
Oparyshev Eugene <laeda@list.ru>
2015-11-17 16:29:46 +0200

    Added contributors to VolleyContext

--- VolleyManagement.Data.MsSql/Context/Migrations/VolleyContextConfiguration.cs
+++ VolleyManagement.Data.MsSql/Context/Migrations/VolleyContextConfiguration.cs
@@ -36,6 +36,405 @@ namespace VolleyManagement.Data.MsSql.Context.Migrations
             context.Roles.AddOrUpdate(
                 r => r.Name,
                 defaultRoles.ToArray());
+
+            context.ContributorTeam.AddOrUpdate(
+                i => i.Name,
+                new ContributorTeamEntity
+                {
+                    Name = "Project Management",
+                    CourseDirection = "All",
+                    Contributors = 
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Sergii Diachenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Iurii Osipchuk",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Oleksii Konovalenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Pavlo Ignatenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Dmytro Petin",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Oleg Shvets",
+                        },
+                     }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-042",
+                    CourseDirection = ".NET",
+                    Contributors = 
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Igor Leta",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Dmytro Balayev",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Ielyzaveta Kalinchuk",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Oleg Petrushov",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Evgenij Kozhan",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-052",
+                    CourseDirection = ".NET",
+                    Contributors = 
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Julia Bukova",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Alexandr Marchotko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Pavlo Dragoetskij",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Anastacia Necheporenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Egor Meshcheriakov",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Pavel Goncharenko",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-061",
+                    CourseDirection = ".NET",
+                    Contributors = 
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Nikita Gordienko",
+                        },
+
+                        new ContributorEntity
+                        {
+                         Name = "Marina Gaponova",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Sofia Shaposhnik",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Pavlo Pokhylenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Sergii Kuzmin",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-064",
+                    CourseDirection = ".NET",
+                    Contributors =
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Andrii Zelyk",
+                        },
+                        new ContributorEntity
+                        {
+                            Name = "Viktoriia Ryndina",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Alex Lapin",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Dmytro Chernyshov",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Maria Kochetkova",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Oleh Vovkodav",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Alexandr Maha",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-064",
+                    CourseDirection = "ATQC",
+                    Contributors = 
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Mykola Kolisnyk",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Maria Tsymbaliuk",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Ipatov Sergii",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Dmitriy Otrashevskiy",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Ruslan Borisenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Zavizion Stanislav",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Oleksandr Rudyi",
+                        },
+                
+                        new ContributorEntity
+                        {
+                            Name = "Sergey Bondarenko",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-065",
+                    CourseDirection = "UI",
+                    Contributors = 
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Denis Chernysh",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Oleh Man'kov",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Kateryna Nikolaieva",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Stanislav Makhnyts'kyi",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Yevhen Alf'orov",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-070",
+                    CourseDirection = "UI",
+                    Contributors =
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Dykhov Egor",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Shafarenko Ol'ha",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Ovcharenko Ekateryna",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Korostienko Daniil",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-072",
+                    CourseDirection = ".NET",
+                    Contributors =
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Denis Rudenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Mykyta Kurchenkov",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Elyzaveta Rudakova",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Mihail Zazharskiy",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Dmitriy Ryndin",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Dmitriy Shapoval",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-076",
+                    CourseDirection = "ATQC",
+                    Contributors =
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Alla Prykhodchenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Andriy Lantukh",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Oleksandr Zaitsev",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Artem Pychenko",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Dmytro Maslov",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Artem  Pozdeev",
+                        },
+                    }
+                },
+                new ContributorTeamEntity
+                {
+                    Name = "Dp-085",
+                    CourseDirection = ".NET",
+                    Contributors =
+                    {
+                        new ContributorEntity
+                        {
+                            Name = "Ievgen Oparyshev",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Mariia Shvets",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Eugene Gerbut",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Pavel Novichkhin",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Artem Kolisnyk",
+                        },
+
+                        new ContributorEntity
+                        {
+                            Name = "Oleksii Khloptsev",
+                        },
+                    }
+                });
         }
 
         private static RoleEntity CreateRole(string name)

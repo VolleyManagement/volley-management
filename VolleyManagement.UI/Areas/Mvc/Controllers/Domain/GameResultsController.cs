daf33216e131f7fc6b4a76a0ced8c7ee7d90508b
Gerbut Evgeny <gekas@i.ua>
2015-12-02 15:06:33 +0200

    Separeted SetScores fields moved to the List, changed views, comparers

--- VolleyManagement.UI/Areas/Mvc/Controllers/Domain/GameResultsController.cs
+++ VolleyManagement.UI/Areas/Mvc/Controllers/Domain/GameResultsController.cs
@@ -6,6 +6,7 @@
     using System.Web;
     using System.Web.Mvc;
     using Contracts;
+    using Domain.GameResultsAggregate;
     using ViewModels.GameResults;
     using ViewModels.Teams;
 
--- VolleyManagement.UI/Areas/Mvc/ViewModels/GameResults/GameResultViewModel.cs
+++ VolleyManagement.UI/Areas/Mvc/ViewModels/GameResults/GameResultViewModel.cs
@@ -1,13 +1,23 @@
 ﻿namespace VolleyManagement.UI.Areas.Mvc.ViewModels.GameResults
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Domain.GameResultsAggregate;
-
+    
     /// <summary>
     /// Game result view model.
     /// </summary>
     public class GameResultViewModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameResultViewModel"/> class.
+        /// </summary>
+        public GameResultViewModel()
+        {
+            this.SetScores = Enumerable.Repeat(new Score(), 5).ToList();
+        }
+
         /// <summary>
         /// Gets or sets the identifier of game result.
         /// </summary>
@@ -24,7 +34,7 @@
         public int HomeTeamId { get; set; }
 
         /// <summary>
-        /// Gets or sets the home team name which played the game.
+        /// Gets or sets name of the home team which played the game.
         /// </summary>
         public string HomeTeamName { get; set; }
 
@@ -34,74 +44,24 @@
         public int AwayTeamId { get; set; }
 
         /// <summary>
-        /// Gets or sets the away team name which played the game.
+        /// Gets or sets name of the away team which played the game.
         /// </summary>
         public string AwayTeamName { get; set; }
 
         /// <summary>
-        /// Gets or sets the final score of the game for the home team.
+        /// Gets or sets the final score of the game.
         /// </summary>
-        public byte HomeSetsScore { get; set; }
+        public Score SetsScore { get; set; }
 
         /// <summary>
-        /// Gets or sets the final score of the game for the away team.
-        /// </summary>
-        public byte AwaySetsScore { get; set; }
-
-        /// <summary>
-        /// Gets or set a value indicating whether the technical defeat has taken place.
+        /// Gets or sets a value indicating whether the technical defeat has taken place.
         /// </summary>
         public bool IsTechnicalDefeat { get; set; }
 
         /// <summary>
-        /// Gets or sets the score of the first set for the home team.
+        /// Gets or sets the set scores.
         /// </summary>
-        public byte HomeSet1Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the first set for the away team.
-        /// </summary>
-        public byte AwaySet1Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the second set for the home team.
-        /// </summary>
-        public byte HomeSet2Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the second set for the away team.
-        /// </summary>
-        public byte AwaySet2Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the third set for the home team.
-        /// </summary>
-        public byte HomeSet3Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the third set for the away team.
-        /// </summary>
-        public byte AwaySet3Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the fourth set for the home team.
-        /// </summary>
-        public byte HomeSet4Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the fourth set for the away team.
-        /// </summary>
-        public byte AwaySet4Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the fifth set for the home team.
-        /// </summary>
-        public byte HomeSet5Score { get; set; }
-
-        /// <summary>
-        /// Gets or sets the score of the fifth set for the away team.
-        /// </summary>
-        public byte AwaySet5Score { get; set; }
+        public List<Score> SetScores { get; set; }
 
         /// <summary>
         /// Maps domain entity to <see cref="GameResultViewModel"/>.
@@ -110,26 +70,18 @@
         /// <returns><see cref="GameResultViewModel"/> entity.</returns>
         public static GameResultViewModel Map(GameResult domainEntity)
         {
-            return new GameResultViewModel()
+            var gameResultViewModel = new GameResultViewModel()
             {
                 Id = domainEntity.Id,
                 TournamentId = domainEntity.TournamentId,
                 HomeTeamId = domainEntity.HomeTeamId,
                 AwayTeamId = domainEntity.AwayTeamId,
-                HomeSetsScore = domainEntity.SetsScore.Home,
-                AwaySetsScore = domainEntity.SetsScore.Away,
+                SetsScore = new Score(domainEntity.SetsScore.Home, domainEntity.SetsScore.Away),
                 IsTechnicalDefeat = domainEntity.IsTechnicalDefeat,
-                HomeSet1Score = domainEntity.SetScores[0].Home,
-                AwaySet1Score = domainEntity.SetScores[0].Away,
-                HomeSet2Score = domainEntity.SetScores[1].Home,
-                AwaySet2Score = domainEntity.SetScores[1].Away,
-                HomeSet3Score = domainEntity.SetScores[2].Home,
-                AwaySet3Score = domainEntity.SetScores[2].Away,
-                HomeSet4Score = domainEntity.SetScores[3].Home,
-                AwaySet4Score = domainEntity.SetScores[3].Away,
-                HomeSet5Score = domainEntity.SetScores[4].Home,
-                AwaySet5Score = domainEntity.SetScores[4].Away
             };
+            gameResultViewModel.SetScores.Clear();
+            domainEntity.SetScores.ForEach(sc => gameResultViewModel.SetScores.Add(new Score(sc.Home, sc.Away)));
+            return gameResultViewModel;
         }
 
         /// <summary>
@@ -138,22 +90,16 @@
         /// <returns>Domain <see cref="GameResult"/> entity.</returns>
         public GameResult ToDomain()
         {
-            return new GameResult()
+            var gameResult = new GameResult()
             {
                 TournamentId = this.TournamentId,
                 HomeTeamId = this.HomeTeamId,
                 AwayTeamId = this.AwayTeamId,
-                SetsScore = new Score(this.HomeSetsScore, this.AwaySetsScore),
+                SetsScore = new Score(this.SetsScore.Home, this.SetsScore.Away),
                 IsTechnicalDefeat = this.IsTechnicalDefeat,
-                SetScores = new List<Score>
-                {
-                    new Score(this.HomeSet1Score, this.AwaySet1Score),
-                    new Score(this.HomeSet2Score, this.AwaySet2Score),
-                    new Score(this.HomeSet3Score, this.AwaySet3Score),
-                    new Score(this.HomeSet4Score, this.AwaySet4Score),
-                    new Score(this.HomeSet5Score, this.AwaySet5Score)
-                }
             };
+            SetScores.ForEach(sc => gameResult.SetScores.Add(new Score(sc.Home, sc.Away)));
+            return gameResult;
         }
     }
 }

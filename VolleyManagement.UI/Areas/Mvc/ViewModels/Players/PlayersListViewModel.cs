8801066fbd8de97adf9760456ff25099303cf70b
Nat Phinik <nb_keep_rockin@mail.ru>
2016-04-01 12:52:27 +0300

    no message

--- VolleyManagement.UI/Areas/Mvc/ViewModels/Players/PlayersListViewModel.cs
+++ VolleyManagement.UI/Areas/Mvc/ViewModels/Players/PlayersListViewModel.cs
@@ -33,8 +33,6 @@
             }
 
             List<PlayerViewModel> listOfPlayers = new List<PlayerViewModel>(source.Skip((this.PageNumber - 1) * Size)
-                            .Where(p => (p.LastName + " " + p.FirstName).Contains(textToSearch)
-                            || (p.FirstName + " " + p.LastName).Contains(textToSearch))
                             .Take(Size)
                             .ToList()
                             .Select(p => PlayerViewModel.Map(p)));

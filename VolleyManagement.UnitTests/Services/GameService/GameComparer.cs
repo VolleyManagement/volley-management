8d2825c7ad5dc84b516adc2d8aebfb6243e1366b
Nick Bocharsky <nickbocharsky@gmail.com>
2016-04-03 12:53:11 +0300

    Change namespaces in ScoreComparer. Move files to GameService folder and rename them. Change names according new domain model. Change usage of Game and Result according new domain model.

--- VolleyManagement.UnitTests/Services/GameService/GameComparer.cs
+++ VolleyManagement.UnitTests/Services/GameService/GameComparer.cs
@@ -1,38 +1,38 @@
-﻿namespace VolleyManagement.UnitTests.Services.GameResultService
+﻿namespace VolleyManagement.UnitTests.Services.GameService
 {
     using System.Collections;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
-    using VolleyManagement.Domain.GameResultsAggregate;
+    using VolleyManagement.Domain.GamesAggregate;
 
     /// <summary>
-    /// Represents a comparer for <see cref="GameResult"/> objects.
+    /// Represents a comparer for <see cref="Game"/> objects.
     /// </summary>
     [ExcludeFromCodeCoverage]
-    internal class GameResultComparer : IComparer<GameResult>, IComparer
+    internal class GameComparer : IComparer<Game>, IComparer
     {
         /// <summary>
-        /// Compares two <see cref="GameResult"/> objects.
+        /// Compares two <see cref="Game"/> objects.
         /// </summary>
         /// <param name="x">The first object to compare.</param>
         /// <param name="y">The second object to compare.</param>
-        /// <returns>A signed integer that indicates the relative values of <see cref="GameResult"/> x and y.</returns>
-        public int Compare(GameResult x, GameResult y)
+        /// <returns>A signed integer that indicates the relative values of <see cref="Game"/> x and y.</returns>
+        public int Compare(Game x, Game y)
         {
             return AreEqual(x, y) ? 0 : 1;
         }
 
         /// <summary>
-        /// Compares two <see cref="GameResult"/> objects (non-generic implementation).
+        /// Compares two <see cref="Game"/> objects (non-generic implementation).
         /// </summary>
         /// <param name="x">The first object to compare.</param>
         /// <param name="y">The second object to compare.</param>
-        /// <returns>A signed integer that indicates the relative values of <see cref="GameResult"/> x and y.</returns>
+        /// <returns>A signed integer that indicates the relative values of <see cref="Game"/> x and y.</returns>
         public int Compare(object x, object y)
         {
-            GameResult firstGameResult = x as GameResult;
-            GameResult secondGameResult = y as GameResult;
+            Game firstGameResult = x as Game;
+            Game secondGameResult = y as Game;
 
             if (firstGameResult == null)
             {
@@ -47,20 +47,22 @@
         }
 
         /// <summary>
-        /// Finds out whether two <see cref="GameResult"/> objects are equal.
+        /// Finds out whether two <see cref="Game"/> objects are equal.
         /// </summary>
         /// <param name="x">The first object to compare.</param>
         /// <param name="y">The second object to compare.</param>
-        /// <returns>True if given <see cref="GameResult"/> objects are equal.</returns>
-        internal bool AreEqual(GameResult x, GameResult y)
+        /// <returns>True if given <see cref="Game"/> objects are equal.</returns>
+        internal bool AreEqual(Game x, Game y)
         {
             return x.Id == y.Id
                 && x.TournamentId == y.TournamentId
                 && x.HomeTeamId == y.HomeTeamId
                 && x.AwayTeamId == y.AwayTeamId
-                && new ScoreComparer().Equals(x.SetsScore, y.SetsScore)
-                && x.IsTechnicalDefeat == y.IsTechnicalDefeat
-                && x.SetScores.SequenceEqual(y.SetScores, new ScoreComparer());
+                && new ScoreComparer().Equals(x.Result.SetsScore, y.Result.SetsScore)
+                && x.Result.IsTechnicalDefeat == y.Result.IsTechnicalDefeat
+                && x.Result.SetScores.SequenceEqual(y.Result.SetScores, new ScoreComparer())
+                && x.GameDate == y.GameDate
+                && x.Round == y.Round;
         }
     }
 }

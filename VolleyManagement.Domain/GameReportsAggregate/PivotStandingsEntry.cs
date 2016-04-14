9bc9e31a09b8014bf34f1fb5c57472d2bd86c31c
Anatolii Padenko <tolik.padenko@gmail.com>
2016-04-14 19:37:57 +0300

    Updated IGameReportService. Added PivotStandingsEntry model.

---
+++ VolleyManagement.Domain/GameReportsAggregate/PivotStandingsEntry.cs
@@ -0,0 +1,40 @@
+﻿namespace VolleyManagement.Domain.GameReportsAggregate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GamesAggregate;
+
+    /// <summary>
+    /// Represents a single entry in pivot tournament's standings.
+    /// </summary>
+    public class PivotStandingsEntry
+    {
+        /// <summary>
+        /// Gets or sets the team's identifier.
+        /// </summary>
+        public int TeamId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the team's name.
+        /// </summary>
+        public string TeamName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of point for the team.
+        /// </summary>
+        public int Points { get; set; }
+
+        /// <summary>
+        /// Gets or sets team result set scores.
+        /// </summary>
+        public List<Result> ResultSetScores { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ratio of number of sets the team won to number of sets the team lost.
+        /// </summary>
+        public float SetsRatio { get; set; }
+    }
+}

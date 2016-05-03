6e6c66ebf49bfbaae77a3aa9297b1e7eae36c5d6
Anatolii Padenko <tolik.padenko@gmail.com>
2016-05-03 18:29:36 +0300

    Added necessary ViewModels.

---
+++ VolleyManagement.UI/Areas/WebAPI/ViewModels/GameReports/PivotStandingsViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace VolleyManagement.UI.Areas.WebApi.ViewModels.GameReports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using VolleyManagement.Domain.GameReportsAggregate;
+
+    /// <summary>
+    /// Represents a view model for <see cref="PivotStandingsViewModel"/>.
+    /// </summary>
+    public class PivotStandingsViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PivotStandingsViewModel"/> class
+        /// </summary>
+        /// <param name="pivotStandings">Instance of a class which implements<see cref="PivotStandingsDto"/></param>
+        public PivotStandingsViewModel(PivotStandingsDto pivotStandings)
+        {
+            TeamsStandings = pivotStandings.Teams.Select(t => PivotStandingsEntryViewModel.Map(t)).ToList();
+            GamesStandings = pivotStandings.GameResults.Select(g => PivotStandingsGamesViewModel.Map(g)).ToList();
+
+            foreach (var game in GamesStandings)
+            {
+                pivotStandings.GameResults
+                    .Where(g => g.HomeTeamId == game.HomeTeamId && g.AwayTeamId == game.AwayTeamId)
+                    .Select(g => ShortGameResultViewModel.Map(g)).ToList();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the ordered collection of teams.
+        /// </summary>
+        public List<PivotStandingsEntryViewModel> TeamsStandings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of games in tournament standings for pivot table.
+        /// </summary>
+        public List<PivotStandingsGamesViewModel> GamesStandings { get; set; }
+    }
+}
---
+++ VolleyManagement.UI/Areas/WebAPI/ViewModels/GameReports/ShortGameResultViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace VolleyManagement.UI.Areas.WebApi.ViewModels.GameReports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using Domain.GameReportsAggregate;
+
+    /// <summary>
+    /// Represents a result part of view model for <see cref="ShortGameResultDto"/>.
+    /// </summary>
+    public class ShortGameResultViewModel
+    {
+        /// <summary>
+        /// Gets or sets the final score of the game for the home team.
+        /// </summary>
+        public byte? HomeSetsScore { get; set; }
+
+        /// <summary>
+        /// Gets or sets the final score of the game for the away team.
+        /// </summary>
+        public byte? AwaySetsScore { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the technical defeat has taken place.
+        /// </summary>
+        public bool IsTechnicalDefeat { get; set; }
+
+        /// <summary>
+        /// Maps domain model of <see cref="ShortGameResultDto"/> to view model <see cref="ShortGameResultViewModel"/>.
+        /// </summary>
+        /// <param name="gameResult">Domain model of <see cref="ShortGameResultDto"/>.</param>
+        /// <returns>View model <see cref="ShortGameResultViewModel"/>.</returns>
+        public static ShortGameResultViewModel Map(ShortGameResultDto gameResult)
+        {
+            return new ShortGameResultViewModel
+            {
+                HomeSetsScore = gameResult.HomeSetsScore,
+                AwaySetsScore = gameResult.AwaySetsScore,
+                IsTechnicalDefeat = gameResult.IsTechnicalDefeat
+            };
+        }
+    }
+}

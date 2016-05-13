955d4ff2144d85d5c245392a6580d6f302930525
Shapoval Dmitriy <tesaurus@ua.fm>
2016-05-13 12:24:04 +0300

    Fix active menu item, review changes

--- VolleyManagement.UI/Scripts/VMScripts/Admin/admin.js
+++ VolleyManagement.UI/Scripts/VMScripts/Admin/admin.js
@@ -26,11 +26,12 @@ $(function() {
         }
     });
 
-    var url = window.location;
+    /*var url = window.location;
     var element = $('ul.nav a').filter(function() {
         return this.href == url || url.href.indexOf(this.href) == 0;
     }).addClass('active').parent().parent().addClass('in').parent();
+
     if (element.is('li')) {
         element.addClass('active');
-    }
+    }*/
 });

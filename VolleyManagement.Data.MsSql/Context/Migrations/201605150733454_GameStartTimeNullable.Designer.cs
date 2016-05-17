df56556a73080aacbad1a7587fff4cdc62137387
Max Prisich <max.prisich@gmail.com>
2016-05-17 19:17:06 +0300

    Migrations fix

--- VolleyManagement.Data.MsSql/Context/Migrations/201605150733454_GameStartTimeNullable.Designer.cs
+++ VolleyManagement.Data.MsSql/Context/Migrations/201605150733454_GameStartTimeNullable.Designer.cs
@@ -13,7 +13,7 @@ namespace VolleyManagement.Data.MsSql.Context.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201605150557171_GameStartTimeNullable"; }
+            get { return "201605150733454_GameStartTimeNullable"; }
         }
         
         string IMigrationMetadata.Source

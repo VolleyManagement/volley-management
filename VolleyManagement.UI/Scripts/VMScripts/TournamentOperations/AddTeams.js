b32604d072da679d68abcb1077f1a282bf876464
Dima Chepa <dimachepa97@mail.ru>
2016-10-09 20:54:09 +0300

    Rewrite AddTeams.js

--- VolleyManagement.UI/Scripts/VMScripts/TournamentOperations/AddTeams.js
+++ VolleyManagement.UI/Scripts/VMScripts/TournamentOperations/AddTeams.js
@@ -20,7 +20,7 @@
 
     privates.getJsonForTournamentTeamsSave = function () {
         var result = {
-            TournamentId: location.search.split('tournamentId=')[1],
+            TournamentId: $('#tournamentId').html(),
             List: []
         }
         var selectedTeams = $("select :selected");
@@ -81,7 +81,7 @@
 
     currNs.onDeleteTeamFromTournamentButtonClick = function (eventData) {
         var teamId = eventData.target.id;
-        var tournamentId = $("[name='TournamentId']").val();
+        var tournamentId = $('#tournamentId').html();
         var confirmDeleteTeamResult = confirm(currNs.deleteConfirmMessage);
         if (confirmDeleteTeamResult) {
             $.post("/Tournaments/DeleteTeamFromTournament",

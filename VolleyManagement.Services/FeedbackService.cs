b80eb9e9719d75793d8e6b012e712f25e67d9532
Dima Chepa <dimachepa97@mail.ru>
2016-10-17 22:54:41 +0300

    Create FeedbackService

--- VolleyManagement.Services/FeedbackService.cs
+++ VolleyManagement.Services/FeedbackService.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VolleyManagement.Services
+﻿namespace VolleyManagement.Services
 {
-    public class FeedbackService
+    using Contracts;
+    using Domain.FeedbackAggregate;
+    using System;
+
+    /// <summary>
+    /// Represents an implementation of IFeedbackService contract.
+    /// </summary>
+    public class FeedbackService : IFeedbackService
     {
+        #region Fields
+
+        private readonly IFeedbackRepository _feedbackService;
+
+        #endregion
+
+        #region Constructor
+
+        public FeedbackService(IFeedbackRepository feedbackService)
+        {
+            _feedbackService = feedbackService;
+        }
+
+        #endregion
+
+        #region Implementation
+
+        /// <summary>
+        /// Creates feedback.
+        /// </summary>
+        /// <param name="feedbackToCreate">Feedback to create.</param>
+        public void Create(Feedback feedbackToCreate)
+        {
+            if (feedbackToCreate == null)
+            {
+                throw new ArgumentNullException("feedback");
+            }
+            UpdateFeedbackTime(feedbackToCreate);
+            _feedbackService.Add(feedbackToCreate);
+            _feedbackService.UnitOfWork.Commit();
+        }
+
+        #endregion
+
+        #region Privates
+
+        private void UpdateFeedbackTime(Feedback feedbackToUpdate)
+        {
+            feedbackToUpdate.Date = DateTime.Now;
+        }
+
+        #endregion
     }
 }

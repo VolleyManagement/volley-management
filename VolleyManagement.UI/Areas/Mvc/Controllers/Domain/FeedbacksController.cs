29d1da65b6f9c2e5f7984203638b39d0bd8a97a0
Nataliia Karasevych <Stihia94@gmail.com>
2016-10-17 22:06:35 +0300

    Added basic architecture

---
+++ VolleyManagement.UI/Areas/Mvc/Controllers/Domain/FeedbacksController.cs
@@ -0,0 +1,31 @@
+﻿namespace VolleyManagement.UI.Areas.Mvc.Controllers
+{
+    using System.Web.Mvc;
+    using ViewModels.FeedbackViewModel;
+
+    /// <summary>
+    /// Defines feedback controller.
+    /// </summary>
+    public class FeedbacksController : Controller
+    {
+        /// <summary>
+        /// Create feedback action GET.
+        /// </summary>
+        /// <returns>Feedback creation view</returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Create feedback action POST.
+        /// </summary>
+        /// <param name="feedbackViewModel">Feedback view model</param>
+        /// <returns>Feedback creation view</returns>
+        [HttpPost]
+        public ActionResult Create(FeedbackViewModel feedbackViewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ VolleyManagement.UI/Areas/Mvc/ViewModels/FeedbackViewModel/FeedbackViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace VolleyManagement.UI.Areas.Mvc.ViewModels.FeedbackViewModel
+{
+    /// <summary>
+    /// Represents a view model for <see cref="FeedbackViewModel"/>.
+    /// </summary>
+    public class FeedbackViewModel
+    {
+        /// <summary>
+        /// Gets or sets feedback Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets users Email.
+        /// </summary>
+        public string UsersEmail { get; set; }
+
+        /// <summary>
+        /// Gets or sets feedback content.
+        /// </summary>
+        public string Content { get; set; }
+    }
+}

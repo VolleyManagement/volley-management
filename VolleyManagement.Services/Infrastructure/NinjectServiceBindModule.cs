e533613c4a1deac4e2f05031c6998193b815ec29
Dima Chepa <dimachepa97@mail.ru>
2016-10-28 22:11:18 +0300

    fix ninject changes

--- VolleyManagement.Services/Infrastructure/NinjectServiceBindModule.cs
+++ VolleyManagement.Services/Infrastructure/NinjectServiceBindModule.cs
@@ -45,8 +45,7 @@
                                   Bind<IRolesService>().To<RolesService>(),
                                   Bind<IGameService>().To<GameService>(),
                                   Bind<IGameReportService>().To<GameReportService>(),
-                                  Bind<IAuthorizationService>().To<AuthorizationService>(),
-                                  Bind<IFeedbackService>().To<FeedbackService>()
+                                  Bind<IAuthorizationService>().To<AuthorizationService>()
                               };
             configs.InScope(_scopeCallback);
         }

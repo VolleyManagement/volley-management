c4354a1175fa2f80e1f1dcd97146188b73104dbe
Geiko <k.i.geiko@gmail.com>
2016-11-09 23:08:44 +0200

    Embed  Ajax

--- VolleyManagement.UI/Scripts/VMScripts/PlayerOperations/LinkPlayerToUser.js
+++ VolleyManagement.UI/Scripts/VMScripts/PlayerOperations/LinkPlayerToUser.js
@@ -1,17 +1,19 @@
-﻿$(document).ready(function () {
+﻿;$(document).ready(function () {
     $("#linkUser").click(function () {
-        
+
+        var playerId = $('#Model_Id').val();
+        var playerFirstName = $('#playerFirstName').html();
+        var playerLastName = $('#playerLastName').html();
 
         $.ajax({
             url: "/Players/LinkWithUser",
             datatype: 'json',
-            data: { id: '@Model.Model.Id' },
+            data: { id: playerId },
             success: function (data) {
                 $('#ajaxResultMessage')
-                    .html(
-                    '<br />' + data +
-                    '<mark><strong><em> @Model.Model.FirstName @Model.Model.LastName</em></strong></mark>'
-                    );
+                    .html('<br />' + data +
+                    '<mark><strong><em> ' + playerFirstName +
+                    ' ' + playerLastName + '</em></strong></mark>');
             },
             error: function () {
                 $('#ajaxResultMessage').html('ERROR');

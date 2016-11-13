3a65ac5c1e297775b9ed4917b684ca44d7b6c731
Geiko <k.i.geiko@gmail.com>
2016-11-13 23:18:56 +0200

    Add UTs

---
+++ VolleyManagement.UnitTests/Services/UserService/BlockUserComparer.cs
@@ -0,0 +1,66 @@
+﻿namespace VolleyManagement.UnitTests.Services.UserService
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using Domain.UsersAggregate;
+
+    /// <summary>
+    /// Comparer for user objects.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    internal class BlockUserComparer : IComparer<User>, IComparer
+    {
+        /// <summary>
+        /// Compare two user objects.
+        /// </summary>
+        /// <param name="x">First user to compare.</param>
+        /// <param name="y">Second user to compare.</param>
+        /// <returns>A signed integer that indicates
+        /// the relative values of users.</returns>
+        public int Compare(User x, User y)
+        {
+            return AreEqual(x, y) ? 0 : 1;
+        }
+
+        /// <summary>
+        /// Compare two user objects (non-generic implementation).
+        /// </summary>
+        /// <param name="x">First user to compare.</param>
+        /// <param name="y">Second user to compare.</param>
+        /// <returns>A signed integer that indicates
+        /// the relative values of users.</returns>
+        public int Compare(object x, object y)
+        {
+            User firstUser = x as User;
+            User secondUser = y as User;
+
+            if (firstUser == null)
+            {
+                return -1;
+            }
+
+            if (secondUser == null)
+            {
+                return 1;
+            }
+
+            return Compare(firstUser, secondUser);
+        }
+
+        /// <summary>
+        /// Finds out whether two user objects have the same properties.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>True if given users have the same properties.</returns>
+        private bool AreEqual(User x, User y)
+        {
+            return x.Id == y.Id
+                && x.Email == y.Email
+                && x.UserName == y.UserName
+                && x.PersonName == y.PersonName
+                && x.IsUserBlocked == y.IsUserBlocked;
+        }
+    }
+}
---
+++ VolleyManagement.UnitTests/Services/UserService/BlockUserServiceTests.cs
@@ -0,0 +1,160 @@
+﻿namespace VolleyManagement.UnitTests.Services.UserService
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using Data.Contracts;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Ninject;
+    using VolleyManagement.Contracts;
+    using VolleyManagement.Contracts.Exceptions;
+    using VolleyManagement.Data.Queries.Common;
+    using VolleyManagement.Domain.UsersAggregate;
+    using VolleyManagement.UI.Infrastructure;
+
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class BlockUserServiceTests
+    {
+        private const int INVALID_USER_ID = -1;
+        private const int VALID_USER_ID = 1;
+        private const string TEST_NAME = "Test Name";
+
+        private readonly Mock<IUserRepository> _userRepositoryMock =
+            new Mock<IUserRepository>();
+
+        private readonly Mock<IUserService> _userServiceMock = new Mock<IUserService>();
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>();
+        private readonly Mock<IQuery<User, FindByIdCriteria>> _getUserByIdQueryMock =
+            new Mock<IQuery<User, FindByIdCriteria>>();
+
+        private IKernel _kernel;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _kernel = new StandardKernel();
+            _kernel.Bind<IUserService>().ToConstant(_userServiceMock.Object);
+            _kernel.Bind<IUserRepository>().ToConstant(_userRepositoryMock.Object);
+            _userRepositoryMock.Setup(fr => fr.UnitOfWork)
+                .Returns(_unitOfWorkMock.Object);
+            _kernel.Bind<IQuery<User, FindByIdCriteria>>()
+                .ToConstant(_getUserByIdQueryMock.Object);
+        }
+
+        [TestMethod]
+        public void SetUserBlocked_UserExist_UpdatedUserReturned()
+        {
+            // Arrange
+            var user = new BlockUserBuilder().WithId(VALID_USER_ID).Build();
+            MockCurrentUser(user, VALID_USER_ID);
+            var sut = _kernel.Get<UserService>();
+
+            // Act
+            sut.SetUserBlocked(VALID_USER_ID);
+
+            // Assert
+            VerifyEditUser(user, Times.Once());
+        }
+
+        [TestMethod]
+        public void SetUserBlocked_UserDoesNotExist_ExceptionThrown()
+        {
+            // Arrange
+            Exception exception = null;
+            MockUserServiceThrowsException(INVALID_USER_ID);
+            var sut = _kernel.Get<UserService>();
+
+            // Act
+            try
+            {
+                sut.SetUserBlocked(INVALID_USER_ID);
+            }
+            catch (MissingEntityException ex)
+            {
+                exception = ex;
+            }
+
+            // Assert
+            VerifyExceptionThrown(
+                exception,
+                "A user with specified identifier was not found");
+        }
+
+        [TestMethod]
+        public void SetUserUnblocked_UserExist_UpdatedUserReturned()
+        {
+            // Arrange
+            var user = new BlockUserBuilder().WithId(VALID_USER_ID).Build();
+            MockCurrentUser(user, VALID_USER_ID);
+            var sut = _kernel.Get<UserService>();
+
+            // Act
+            sut.SetUserUnblocked(VALID_USER_ID);
+
+            // Assert
+            VerifyEditUser(user, Times.Once());
+        }
+
+        [TestMethod]
+        public void SetUserUnblocked_UserDoesNotExist_ExceptionThrown()
+        {
+            // Arrange
+            Exception exception = null;
+            MockUserServiceThrowsException(INVALID_USER_ID);
+            var sut = _kernel.Get<UserService>();
+
+            // Act
+            try
+            {
+                sut.SetUserUnblocked(INVALID_USER_ID);
+            }
+            catch (MissingEntityException ex)
+            {
+                exception = ex;
+            }
+
+            // Assert
+            VerifyExceptionThrown(
+                exception,
+                "A user with specified identifier was not found");
+        }
+
+        private void MockCurrentUser(User testData, int userId)
+        {
+            _getUserByIdQueryMock
+                .Setup(tr => tr.Execute(It.IsAny<FindByIdCriteria>()))
+                .Returns(testData);
+        }
+
+        private void VerifyEditUser(User user, Times times)
+        {
+            _userRepositoryMock.Verify(
+                ur => ur.Update(It.Is<User>(u => UsersAreEqual(u, user))),
+                times);
+            _unitOfWorkMock.Verify(uow => uow.Commit(), times);
+        }
+
+        private bool UsersAreEqual(User x, User y)
+        {
+            return new BlockUserComparer().Compare(x, y) == 0;
+        }
+
+        private void VerifyExceptionThrown(
+            Exception exception,
+            string expectedMessage)
+        {
+            Assert.IsNotNull(exception, "There is no exception thrown");
+            Assert.IsTrue(
+                exception.Message.Equals(expectedMessage),
+                "Expected and actual exceptions messages aren't equal");
+        }
+
+        private void MockUserServiceThrowsException(int userId)
+        {
+            _userServiceMock
+                .Setup(tr => tr.GetUser(userId))
+                .Throws<MissingEntityException>();
+        }
+    }
+}

c08ccfbc2cedeaacdc9cb9873a8a66db088ff090
Anastasiia Kolobovnykova <nastena31011993@gmail.com>
2016-11-13 23:13:42 +0200

    Added more UTs

--- VolleyManagement.UnitTests/Mvc/Controllers/RequestControllerTests.cs
+++ VolleyManagement.UnitTests/Mvc/Controllers/RequestControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using VolleyManagement.UnitTests.Services.UsersService;
+﻿using System.Web.Mvc;
+using VolleyManagement.UnitTests.Services.UsersService;
 
 namespace VolleyManagement.UnitTests.Mvc.Controllers
 {
@@ -21,6 +22,7 @@ namespace VolleyManagement.UnitTests.Mvc.Controllers
     using VolleyManagement.UnitTests.Services.UserManager;
     using WebApi.ViewModels;
     using Admin.Comparers;
+    using Contracts.Exceptions;
 
     [ExcludeFromCodeCoverage]
     [TestClass]
@@ -50,35 +52,95 @@ namespace VolleyManagement.UnitTests.Mvc.Controllers
             this._kernel.Bind<IPlayerService>().ToConstant(this._playerServiceMock.Object);
             this._kernel.Bind<IRequestService>().ToConstant(this._requestServiceMock.Object);
             this._httpContextMock.SetupGet(c => c.Request).Returns(this._httpRequestMock.Object);
-            this._sut = _kernel.Get<RequestController>();
+          //  this._sut = _kernel.Get<RequestController>();
         }
 
         #endregion
 
         #region Tests
 
-        [TestMethod]
+       [TestMethod]
         public void UserDetails_ExistingUser_UserViewModelIsReturned()
         {
             // Arrange
-            var requests = GetRequests();
-            _requestServiceMock.Setup(r => r.Get()).Returns(requests);
-
-            var user = new UserBuilder().WithId(EXISTING_ID).Build();
-
-            SetupGetUserDetails(EXISTING_ID, user);
-            var expected = CreateUserViewModel();
+            var expected = GetUser();
+            int id = 1;
+            MockUserService(id);
+            var sut = _kernel.Get<RequestController>();
 
             // Act
-            var actual = TestExtensions.GetModel<UserViewModel>(_sut.UserDetails(EXISTING_ID));
+            var actionResult = sut.UserDetails(id);
+            var actual = TestExtensions.GetModel<User>(actionResult);
 
             // Assert
-            TestHelper.AreEqual<UserViewModel>(expected, actual, new UserViewModelComparer());
+            TestHelper.AreEqual<User>(expected, actual, new UserComparer());
+        }
+
+        [TestMethod]
+        public void UserDetails_NonExistentUser_HttpNotFoundResultIsReturned()
+        {
+            // Arrange
+            const int USER_ID = 1;
+            MockUserServiceReturnsNullUser(USER_ID);
+            var sut = _kernel.Get<RequestController>();
+
+            // Act
+            var actionResult = sut.UserDetails(USER_ID);
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult, typeof(HttpNotFoundResult));
+        }
+
+        [TestMethod]
+        public void Confirm_AnyRequest_RequestRedirectToIndex()
+        {
+            // Arrange
+            const int REQUEST_ID = 1;
+            var controller = _kernel.Get<RequestController>();
+
+            // Act
+            var actionResult = controller.Confirm(REQUEST_ID);
+
+            // Assert
+            AssertValidRedirectResult(actionResult, "Index");
+        }
+
+        [TestMethod] // не нужен?
+        public void Close_AnyRequest_RequestConfirmed()
+        {
+            // Arrange
+            const int REQUEST_ID = 1;
+            var controller = _kernel.Get<RequestController>();
+
+            // Act
+            var actionResult = controller.Confirm(REQUEST_ID);
+
+            // Assert
+            AssertConfirmVerify(_requestServiceMock, REQUEST_ID);
+        }
+
+        [TestMethod]
+        public void Confirm_NonExistentRequest_ThrowsException()
+        {
+            // Arrange
+            const int REQUEST_ID = 1;
+            SetupConfirmThrowsMissingEntityException();
+
+            // Act
+            var result = _sut.Confirm(REQUEST_ID);
+
+            // Assert
+            Assert.IsNotNull(result, "");
         }
 
         #endregion
 
         #region Test data
+
+        private User GetUser()
+        {
+            return new User();
+        }
 
         private static List<Request> GetRequests()
         {
@@ -119,6 +181,37 @@ namespace VolleyManagement.UnitTests.Mvc.Controllers
         }
 
         #endregion
+
+        private static void AssertValidRedirectResult(ActionResult actionResult, string view)
+        {
+            var result = (RedirectToRouteResult)actionResult;
+            Assert.IsFalse(result.Permanent, "Redirect should not be permanent");
+            Assert.AreEqual(1, result.RouteValues.Count, string.Format("Redirect should forward to Requests.{0} action", view));
+            Assert.AreEqual(view, result.RouteValues["action"], string.Format("Redirect should forward to Requests.{0} action", view));
+        }
+
+        private static void AssertConfirmVerify(Mock<IRequestService> mock, int requestId)
+        {
+            mock.Verify(ps => ps.Approve(It.Is<int>(id => id == requestId)), Times.Once());
+        }
+
+        //Assert.AreEqual(expected.Id, actual.Id);
+        private void MockUserService(int id)
+        {
+            this._userServiceMock.Setup(m => m.GetUser(id)).Returns(new User());
+        }
+
+        private void MockUserServiceReturnsNullUser(int id)
+        {
+            User user = null;
+            this._userServiceMock.Setup(m => m.GetUser(id)).Returns(user);
+        }
+
+        private void SetupConfirmThrowsMissingEntityException()
+        {
+            _requestServiceMock.Setup(ts => ts.Get(It.IsAny<int>()))
+                .Throws(new MissingEntityException(string.Empty));
+        }
 
         private void SetupGetUserDetails(int userId, User user)
         {

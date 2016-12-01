f2a30a67cbaec1ae6297a4f6b5e20263f2f6c05e
Vladislav <vlad.antonyuk@gmail.com>
2016-12-01 18:28:33 +0200

    fix conflict

--- VolleyManagement.UI/Areas/Mvc/Controllers/Domain/TeamsController.cs
+++ VolleyManagement.UI/Areas/Mvc/Controllers/Domain/TeamsController.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Data;
     using System.IO;
     using System.Linq;
     using System.Web;

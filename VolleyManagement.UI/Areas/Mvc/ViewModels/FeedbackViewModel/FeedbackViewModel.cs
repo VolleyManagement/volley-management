e4ecbc1ab89bdbbeceb91d580200c242b5a4d7c3
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2016-12-09 14:30:31 +0200

    Fix error: Either ErrorMessageString or ErrorMessageResourceName must be set, but not both

--- VolleyManagement.UI/Areas/Mvc/ViewModels/FeedbackViewModel/FeedbackViewModel.cs
+++ VolleyManagement.UI/Areas/Mvc/ViewModels/FeedbackViewModel/FeedbackViewModel.cs
@@ -21,7 +21,8 @@
         [Required(ErrorMessageResourceName = "FieldRequired",
             ErrorMessageResourceType = typeof(ViewModelResources))]
         [EmailAddress(ErrorMessageResourceName = "InvalidEmail",
-            ErrorMessageResourceType = typeof(ViewModelResources))]
+            ErrorMessageResourceType = typeof(ViewModelResources),
+            ErrorMessage = null)]
         [StringLength(80)]
         public string UsersEmail { get; set; }
 

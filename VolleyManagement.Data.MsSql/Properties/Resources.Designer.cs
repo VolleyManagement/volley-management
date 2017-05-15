fdab5bb1aae42c5a246bb05bc04acef5e56de0b7
Sergii Diachenko <sdiachenko@outlook.com>
2017-05-15 23:06:26 +0300

    Changed target framework

--- VolleyManagement.Data.MsSql/Properties/Resources.Designer.cs
+++ VolleyManagement.Data.MsSql/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace VolleyManagement.Data.Properties {
+namespace VolleyManagement.Data.MsSql.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace VolleyManagement.Data.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VolleyManagement.Data.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VolleyManagement.Data.MsSql.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

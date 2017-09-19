5beefdb822035ac69705b90d8e2a2be037f73e33
Nick Bocharkiy <nickbocharsky@gmail.com>
2017-09-19 16:30:45 +0300

    Modify routing to navigate to pivot standings table.

--- VolleyManagement.UI/src/app/Modules/app-routing.ts
+++ VolleyManagement.UI/src/app/Modules/app-routing.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { PivotStandingsComponent } from '../Components/PivotStanding/pivotstandings.component';
 import { StandingsComponent } from '../Components/Standings/standings.component';
 
 
 const routes: Routes = [
+    { path: 'PivotTable/:id', component: PivotStandingsComponent },
     { path: 'Standings/:id', component: StandingsComponent }
 ];
 

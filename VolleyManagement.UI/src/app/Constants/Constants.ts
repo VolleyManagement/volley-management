bbf24ffc02e274b07d3a6381f89fe603eb67cdea
Nick Bocharkiy <nickbocharsky@gmail.com>
2017-09-19 16:09:17 +0300

    Add new values to Constants class

--- VolleyManagement.UI/src/app/Constants/Constants.ts
+++ VolleyManagement.UI/src/app/Constants/Constants.ts
@@ -1,3 +1,8 @@
 export class Constants {
     static readonly BASE_API_URL = 'https://localhost:44300/api/v1/';
+    static readonly DEFAULT_LOCALE = 'uk-UA';
+    static readonly ZERO = 0;
+    static readonly ONE = 1;
+    static readonly TWO = 2;
+    static readonly THREE = 3;
 }

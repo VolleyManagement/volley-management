6db3ae89fa04487d6e7c4edce373c60f2f0b7f07
Nick Bocharkiy <nickbocharsky@gmail.com>
2017-09-19 16:26:50 +0300

    Add class with css constants

---
+++ VolleyManagement.UI/src/app/Constants/CssClassConstants.ts
@@ -0,0 +1,10 @@
+export class CssClassConstants {
+    public static readonly WIN_3_0 = 'win_3_0';
+    public static readonly WIN_3_1 = 'win_3_1';
+    public static readonly WIN_3_2 = 'win_3_2';
+    public static readonly LOSS_2_3 = 'loss_2_3';
+    public static readonly LOSS_1_3 = 'loss_1_3';
+    public static readonly LOSS_0_3 = 'loss_0_3';
+    public static readonly NON_PLAYABLE_CELL = 'non-playable';
+    public static readonly NORESULT = 'noresult';
+}

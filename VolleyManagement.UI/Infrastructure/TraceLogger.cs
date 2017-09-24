40e1354d29c9fade9477c540290e4ca8490db11d
Sergii Diachenko <sdiachenko@outlook.com>
2017-09-24 21:32:05 +0000

    Fixed logger dependency

---
+++ VolleyManagement.UI/Infrastructure/TraceLogger.cs
@@ -0,0 +1,13 @@
+﻿namespace VolleyManagement.UI.Infrastructure
+{
+    using System.Diagnostics;
+    using Crosscutting.Contracts.Infrastructure;
+
+    public class TraceLogger : ILog
+    {
+        public void Write(LogLevelEnum level, string message)
+        {
+            Trace.Write(message, level.ToString());
+        }
+    }
+}

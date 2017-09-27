a2fdce22812fda15b1311b184d67f54b6e2f5aa9
Nick Bocharkiy <nickbocharsky@gmail.com>
2017-09-27 12:10:42 +0300

    Move methods from PivotStandingsGame class to ShartGameResult.

--- VolleyManagement.UI/src/app/Models/Pivot/PivotStandingsGame.ts
+++ VolleyManagement.UI/src/app/Models/Pivot/PivotStandingsGame.ts
@@ -1,6 +1,4 @@
 import { ShortGameResult } from './ShortGameResult';
-import { CssClassConstants } from '../../Constants/CssClassConstants';
-import { Constants } from '../../Constants/Constants';
 
 export class PivotStandingsGame {
     constructor(
@@ -13,52 +11,7 @@ export class PivotStandingsGame {
         return new PivotStandingsGame(
             0,
             0,
-            new ShortGameResult(
-                0,
-                0,
-                false,
-                CssClassConstants.NON_PLAYABLE_CELL,
-                CssClassConstants.NON_PLAYABLE_CELL));
-    }
-
-    private static getCssClass(homeScore?: number, awayScore?: number): string {
-        let cssClass = CssClassConstants.NORESULT;
-        const setDifference = homeScore - awayScore;
-
-        switch (setDifference) {
-            case 3:
-                cssClass = CssClassConstants.WIN_3_0;
-                break;
-            case 2:
-                cssClass = CssClassConstants.WIN_3_1;
-                break;
-            case 1:
-                cssClass = CssClassConstants.WIN_3_2;
-                break;
-            case -1:
-                cssClass = CssClassConstants.LOSS_2_3;
-                break;
-            case -2:
-                cssClass = CssClassConstants.LOSS_1_3;
-                break;
-            case -3:
-                cssClass = CssClassConstants.LOSS_0_3;
-                break;
-        }
-
-        return cssClass;
-    }
-
-    private static formattedResult(homeSetsScore: number, awaySetsScore: number, isTechnicalDefeat: boolean): string {
-        let result = '';
-        if (homeSetsScore !== Constants.ZERO || awaySetsScore !== Constants.ZERO) {
-            result = homeSetsScore + ' : ' + awaySetsScore;
-            if (isTechnicalDefeat) {
-                result += '*';
-            }
-        }
-
-        return result;
+            ShortGameResult.getNonPlayableCell());
     }
 
     public clone(): PivotStandingsGame {
@@ -68,16 +21,8 @@ export class PivotStandingsGame {
             new ShortGameResult(
                 this.Results.HomeSetsScore,
                 this.Results.AwaySetsScore,
-                this.Results.IsTechnicalDefeat,
-                PivotStandingsGame.formattedResult(
-                    this.Results.HomeSetsScore,
-                    this.Results.AwaySetsScore,
-                    this.Results.IsTechnicalDefeat),
-                PivotStandingsGame.getCssClass(
-                    this.Results.HomeSetsScore,
-                    this.Results.AwaySetsScore)));
+                this.Results.IsTechnicalDefeat));
     }
-
 
     public transposeResult(): PivotStandingsGame {
         return new PivotStandingsGame(
@@ -86,13 +31,6 @@ export class PivotStandingsGame {
             new ShortGameResult(
                 this.Results.AwaySetsScore,
                 this.Results.HomeSetsScore,
-                this.Results.IsTechnicalDefeat,
-                PivotStandingsGame.formattedResult(
-                    this.Results.AwaySetsScore,
-                    this.Results.HomeSetsScore,
-                    this.Results.IsTechnicalDefeat),
-                PivotStandingsGame.getCssClass(
-                    this.Results.AwaySetsScore,
-                    this.Results.HomeSetsScore)));
+                this.Results.IsTechnicalDefeat));
     }
 }

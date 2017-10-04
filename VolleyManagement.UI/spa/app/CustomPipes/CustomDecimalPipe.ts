d1f326db45a5c15fa24a70efef656801017f42ad
Nick Bocharkiy <nickbocharsky@gmail.com>
2017-10-04 14:17:17 +0300

    Add custom pipe for spa

---
+++ VolleyManagement.UI/spa/app/CustomPipes/CustomDecimalPipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+
+@Pipe({
+    name: 'customNumber'
+})
+export class CustomDecimalPipe extends DecimalPipe implements PipeTransform {
+    transform(value: string, digits?: string): any {
+        return value === 'Infinity' ? 'Max' : super.transform(value, digits);
+    }
+}

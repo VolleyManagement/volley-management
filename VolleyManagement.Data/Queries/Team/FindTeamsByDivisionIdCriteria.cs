e67aca5011932a6c5672c15cdf530c466ce2879e
Nick Bocharkiy <nickbocharsky@gmail.com>
2017-10-04 14:18:27 +0300

    Add new class with search criteria(get teams by division)

---
+++ VolleyManagement.Data/Queries/Team/FindTeamsByDivisionIdCriteria.cs
@@ -0,0 +1,12 @@
+﻿namespace VolleyManagement.Data.Queries.Team
+{
+    using Contracts;
+
+    public class FindTeamsByDivisionIdCriteria : IQueryCriteria
+    {
+        /// <summary>
+        /// Gets or sets Division Id to search for
+        /// </summary>
+        public int DivisionId { get; set; }
+    }
+}

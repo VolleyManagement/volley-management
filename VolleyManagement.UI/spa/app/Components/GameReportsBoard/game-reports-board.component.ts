1cc1af8919a363eec3db773fd727ce7e7f21432d
DShapoval <dushapoval@gmail.com>
2017-11-29 17:31:24 +0200

    Dynamic height calculation for loader

--- VolleyManagement.UI/spa/app/Components/GameReportsBoard/game-reports-board.component.ts
+++ VolleyManagement.UI/spa/app/Components/GameReportsBoard/game-reports-board.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 
 import { TournamentMetadataJson } from '../../Models/TournamentMetadataJson/TournamentMetadataJson';
 import { TournamentDataService } from '../../Services/tournament-data.service';
+
 
 @Component({
     selector: 'vm-game-reports-board',
@@ -23,27 +24,59 @@ export class GameReportsBoardComponent implements OnInit {
     standingsId: number;
     scheduleId: number;
 
+    @ViewChild('containerEl')
+    containerEl: ElementRef;
+
+    loaderHeight = 0;
+    isLoaderVisible = true;
+
     get isShowLoader() {
-        return !(this.reportsStates.standings
-            && this.reportsStates.pivot
-            && this.reportsStates.schedule);
+        return !this._allReportsReady();
     }
 
     constructor(
         private _dataService: TournamentDataService) { }
 
     ngOnInit(): void {
+        this._updateHeight();
+
         this._dataService
             .getTournamentMetadata()
             .toPromise()
-            .then(data => this.updateIds(data.mode, data.id));
+            .then(data => {
+                this._updateIds(data.mode, data.id);
+                this._updateLoaderState();
+            });
     }
 
     onReportReady(report: string) {
         this.reportsStates[report] = true;
+
+        // setTimeout needed to allow browser render changes and return valid height for wrapper
+        setTimeout(() => this._updateLoaderState(), 200);
     }
 
-    private updateIds(modes: string[], id: number): void {
+    private _updateHeight() {
+
+        if (!this.containerEl) {
+            this.loaderHeight = 0;
+        }
+
+        this.loaderHeight = this.containerEl.nativeElement.offsetHeight;
+    }
+
+    private _updateLoaderState() {
+        this._updateHeight();
+        this.isLoaderVisible = !this._allReportsReady();
+    }
+
+    private _allReportsReady() {
+        return this.reportsStates.standings
+            && this.reportsStates.pivot
+            && this.reportsStates.schedule;
+    }
+
+    private _updateIds(modes: string[], id: number): void {
         modes.forEach(mode => {
             switch (mode) {
                 case 'pivot':
--- VolleyManagement.UI/spa/app/Components/Loader/loader.component.ts
+++ VolleyManagement.UI/spa/app/Components/Loader/loader.component.ts
@@ -2,18 +2,23 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AppToolsService } from '../../Services/app-tools.service';
 
 @Component({
-  selector: 'loader',
-  templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+    selector: 'loader',
+    templateUrl: './loader.component.html',
+    styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
 
-  @Input() isShowLoader: boolean;
-  public isCss3Supported = false;
+    @Input()
+    visible: boolean;
 
-  constructor(private _appTools: AppToolsService) { }
+    @Input()
+    height: number;
 
-  ngOnInit() {
-    this.isCss3Supported = this._appTools.isCss3Supported();
-  }
+    public isCss3Supported = false;
+
+    constructor(private _appTools: AppToolsService) { }
+
+    ngOnInit() {
+        this.isCss3Supported = this._appTools.isCss3Supported();
+    }
 }

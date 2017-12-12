7da00fc448e60eb4a64342eb79013c3c7b8d4fb5
Sergii Diachenko <sdiachenko@outlook.com>
2017-12-12 10:05:30 +0200

    Fixed angular prod variables

--- VolleyManagement.UI/spa/environments/environment.prod.ts
+++ VolleyManagement.UI/spa/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   // place real api address which will be used in production
-  apiUrl: 'https://localhost:44300/',
-  //apiUrl: 'https://volley-mgmt-app.azurewebsites.net/',
+  //apiUrl: 'https://localhost:44300/',
+  apiUrl: 'https://volley-mgmt-app.azurewebsites.net/',
   jsonBaseUrl: 'https://volleycontent.blob.core.windows.net/vmscripts/tournamentData/'
 };
 

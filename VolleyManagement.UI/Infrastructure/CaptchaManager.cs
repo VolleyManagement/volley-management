f0657d439b33ecbd821312b1e033f6ad334f0469
tolik_padenko <tolik.padenko@gmail.com>
2018-02-07 14:05:24 +0200

    Refactor captcha manager

--- VolleyManagement.UI/Infrastructure/CaptchaManager.cs
+++ VolleyManagement.UI/Infrastructure/CaptchaManager.cs
@@ -5,38 +5,57 @@
     using System.Web.Configuration;
     using Contracts;
     using Newtonsoft.Json.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System;
 
     /// <summary>
     /// Represents an Implementation of ICaptchaManager interface.
     /// </summary>
     public class CaptchaManager : ICaptchaManager
     {
-        private const string SECRET_KEY = "RecaptchaSecretKey";
-        private const string CaptchaResponse = "g-recaptcha-response";
-        private const string CaptchaPath = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}";
-
+        private const string CONFIG_KEY_RECAPTCHA_SECERET = "RecaptchaSecretKey";
+        private const string RECAPTCHA_VERIFY_URL_FORMAT = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}";
+        private const string RECAPTCHA_JSON_TOKEN_SUCCESS = "success";
+        
         /// <summary>
         /// Method, that verifies if captcha is valid
         /// </summary>
-        /// <param name="response">Response of Captcha.</param>
+        /// <param name="userResponseToken">Response of Captcha.</param>
         /// <returns>Captcha result</returns>
-        public bool IsFormSubmit(string response)
+        public async Task<bool> ValidateUserCaptcha(string userResponseToken)
         {
-            // ToDo: Refactor: excpetion handling, naming
-            bool status = false;
-            string secretKey = WebConfigurationManager.AppSettings[SECRET_KEY];
-            using (var client = new WebClient())
+            var isCaptchaValid = false;
+            using (var client = new HttpClient())
             {
-                var result = client.DownloadString(
-                    string.Format(
-                        CaptchaPath,
-                        secretKey,
-                        response));
-                var obj = JObject.Parse(result);
-                status = (bool)obj.SelectToken("success");
+                try
+                {
+                    var jsonResultStr = await client.GetStringAsync(GetCapchaRequestUrl(userResponseToken));
+
+                    var jsonResultObj = JObject.Parse(jsonResultStr);
+                    isCaptchaValid = jsonResultObj[RECAPTCHA_JSON_TOKEN_SUCCESS].Value<bool>();
+                }               
+                catch (HttpRequestException httpException)
+                {
+                    //Means that there are issues with network. LOGGING REQUIRED
+                    isCaptchaValid = false;
+                }
+                catch (ArgumentNullException argumentNullException)
+                {
+                    //Means that response from Google is not JSON. LOGGING REQUIRED
+                    isCaptchaValid = false;
+                }
             }
 
-            return status;
+            return isCaptchaValid;
+        }
+
+        private string GetCapchaRequestUrl(string userResponseToken)
+        {
+            string secretKey = WebConfigurationManager.AppSettings[CONFIG_KEY_RECAPTCHA_SECERET];
+            return string.Format(RECAPTCHA_VERIFY_URL_FORMAT,
+                                 secretKey,
+                                 userResponseToken);
         }
     }
 }

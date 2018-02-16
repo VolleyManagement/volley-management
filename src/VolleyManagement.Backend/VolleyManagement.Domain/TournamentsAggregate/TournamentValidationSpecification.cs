4209fe6a7ce630dbc1a921b6839407ce158e4364
ViznyakS <viznyak.sofia@ukr.net>
2018-02-17 00:19:04 +0200

    Fix IsSatisfiedBy(Tournament)

--- src/VolleyManagement.Backend/VolleyManagement.Domain/TournamentsAggregate/TournamentValidationSpecification.cs
+++ src/VolleyManagement.Backend/VolleyManagement.Domain/TournamentsAggregate/TournamentValidationSpecification.cs
@@ -16,25 +16,25 @@
         public bool IsSatisfiedBy(Tournament entity)
         {
             // Name
-            if (!string.IsNullOrEmpty(entity.Name) && entity.Name.Length <= Constants.Tournament.MAX_NAME_LENGTH)
+            if (string.IsNullOrEmpty(entity.Name) || entity.Name.Length >= Constants.Tournament.MAX_NAME_LENGTH)
             {
                 return false;
             }
             //// Description
-            if (string.IsNullOrEmpty(entity.Description)
-                || entity.Description.Length <= Constants.Tournament.MAX_DESCRIPTION_LENGTH)
+            if (!string.IsNullOrEmpty(entity.Description)
+                && entity.Description.Length >= Constants.Tournament.MAX_DESCRIPTION_LENGTH)
             {
                 return false;
             }
             //// Season
-            if (entity.Season >= Constants.Tournament.MINIMAL_SEASON_YEAR
-                && entity.Season <= Constants.Tournament.MAXIMAL_SEASON_YEAR)
+            if (entity.Season <= Constants.Tournament.MINIMAL_SEASON_YEAR
+                && entity.Season >= Constants.Tournament.MAXIMAL_SEASON_YEAR)
             {
                 return false;
             }
             //// Regulation link
-            if (string.IsNullOrEmpty(entity.RegulationsLink)
-                  || entity.RegulationsLink.Length <= Constants.Tournament.MAX_REGULATION_LENGTH)
+            if (!string.IsNullOrEmpty(entity.RegulationsLink)
+                  && entity.RegulationsLink.Length >= Constants.Tournament.MAX_REGULATION_LENGTH)
             {
                 return false;
             }

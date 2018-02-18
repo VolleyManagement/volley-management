80bb390db738db4bc7414165943c8f650c95c6f6
Myroslavv <myroslav.vyspyansky@gmail.com>
2018-02-18 17:02:54 +0200

    Split IocDataAccessModule.cs functionality into smaller, more specific classes.

---
+++ src/VolleyManagement.Backend/VolleyManagement.Data.MsSql/Infrastructure/QueriesRegistrator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using VolleyManagement.Crosscutting.Contracts.Infrastructure.IOC;
+using VolleyManagement.Data.Contracts;
+using VolleyManagement.Data.MsSql.Queries;
+
+namespace VolleyManagement.Data.MsSql.Infrastructure
+{
+    static class QueriesRegistrator
+    {
+        public static void Register(IIocContainer container)
+        {
+            var queriesAssembly = typeof(TournamentQueries).Assembly;
+
+            var registrations =
+                from type in queriesAssembly.GetExportedTypes()
+                where type.Namespace == "VolleyManagement.Data.MsSql.Queries"
+                where type.GetInterfaces().Any(InterfaceIsQuery)
+                select new
+                {
+                    Contracts = type.GetInterfaces().Where(InterfaceIsQuery),
+                    Implementation = type
+                };
+
+            foreach (var item in registrations)
+            {
+                foreach (var contract in item.Contracts)
+                {
+                    container.Register(contract, item.Implementation, IocLifetimeEnum.Scoped);
+                }
+            }
+        }
+
+        private static bool InterfaceIsQuery(Type type)
+        {
+            var typeDefinition = type.GetGenericTypeDefinition();
+            return typeDefinition == typeof(IQuery<,>) || typeDefinition == typeof(IQueryAsync<,>);
+        }
+    }
+}
---
+++ src/VolleyManagement.Backend/VolleyManagement.Data.MsSql/Infrastructure/RepositoriesRegistrator.cs
@@ -0,0 +1,25 @@
+﻿using VolleyManagement.Crosscutting.Contracts.Infrastructure.IOC;
+using VolleyManagement.Data.Contracts;
+using VolleyManagement.Data.MsSql.Repositories;
+using VolleyManagement.Domain.FeedbackAggregate;
+using VolleyManagement.Domain.GamesAggregate;
+using VolleyManagement.Domain.RequestsAggregate;
+using VolleyManagement.Domain.TournamentRequestAggregate;
+using VolleyManagement.Domain.TournamentsAggregate;
+
+namespace VolleyManagement.Data.MsSql.Infrastructure
+{
+    static class RepositoriesRegistrator
+    {
+        public static void Register(IIocContainer container)
+        {
+            container
+                .Register<IUnitOfWork, VolleyUnitOfWork>(IocLifetimeEnum.Scoped)
+                .Register<ITournamentRepository, TournamentRepository>(IocLifetimeEnum.Scoped)
+                .Register<IGameRepository, GameRepository>(IocLifetimeEnum.Scoped)
+                .Register<IFeedbackRepository, FeedbackRepository>(IocLifetimeEnum.Scoped)
+                .Register<ITournamentRequestRepository, TournamentRequestRepository>(IocLifetimeEnum.Scoped)
+                .Register<IRequestRepository, RequestRepository>(IocLifetimeEnum.Scoped);
+        }
+    }
+}

528fe0845400b0f48b03e1c8ff956807707d027d
NataliiaBidiuk <33187342+NataliiaBidiuk@users.noreply.github.com>
2018-03-01 11:03:24 +0200

    Issue #60 improve defect localization for standings tests (#195)
    * Add TeamStandingsDtoComparerByPoints
    
    * Add 4 test instead of GetPivotStandings_GameResultsAllPossibleScores_CorrectStats
    
    * Change logic in PivotStandingsComparer
    
    * Change AssertPivotStandingsAreEqualByGameStatus
    
    * Change AssertPivotStandingsAreEqual
    
    * Split Standing test on 4 less tests: ByPoints, BySets, ByGames, ByBalls
    
    * rename tests and private fields, add new method WithAllComparer, modify constructor StandingDTOComparer
    
    * Rename fields, change PivotStandingsComparer
    
    * Delete field, rename properties, modify test
    
    * change constructor
    
    * Add enum ComparerBy, change logic in PivotStandingsComparer
    
    * Add method that make false all proporities
    
    * sorry fix 1 syntax mistakes
    
    * Go back to previous version
    
    * delete enum
    
    * sorry, fix errors
    
    * Delete unnecessary using
    
    * Ckeck some little mistakes and remarks
    
    * ctrl+K+D

--- tests/VolleyManagement.UnitTests/Services/GameReportService/Comparers/PivotStandingsComparer.cs
+++ tests/VolleyManagement.UnitTests/Services/GameReportService/Comparers/PivotStandingsComparer.cs
@@ -6,6 +6,40 @@
 
     internal class PivotStandingsComparer : IComparer<PivotStandingsDto>
     {
+        private TeamStandingsDtoComparer teamsComparer;
+        private bool hasComparerByGames = true;
+
+        public PivotStandingsComparer()
+        {
+            this.teamsComparer = new TeamStandingsDtoComparer();
+        }
+        public void WithPointsComparer()
+        {
+            CleanComparerFlags();
+            teamsComparer.HasComparerByPoints = true;
+        }
+        public void WithSetsComparer()
+        {
+            CleanComparerFlags();
+            teamsComparer.HasComparerBySets = true;
+        }
+        public void WithBallsComparer()
+        {
+            CleanComparerFlags();
+            teamsComparer.HasComparerByBalls = true;
+        }
+        public void WithGamesComparer()
+        {
+            CleanComparerFlags();
+            hasComparerByGames = true;
+        }
+        private void CleanComparerFlags()
+        {
+            teamsComparer.HasComparerByPoints = false;
+            teamsComparer.HasComparerByBalls = false;
+            teamsComparer.HasComparerBySets = false;
+            hasComparerByGames = false;
+        }
         public int Compare(PivotStandingsDto x, PivotStandingsDto y)
         {
             Assert.AreEqual(x.DivisionId, y.DivisionId, "Division Ids do not match");
@@ -14,7 +48,6 @@
 
             if (x.Teams.Count == y.Teams.Count)
             {
-                var teamsComparer = new TeamStandingsDtoComparer();
                 for (var i = 0; i < x.Teams.Count; i++)
                 {
                     if (teamsComparer.Compare(x.Teams[i], y.Teams[i]) != 0)
@@ -28,22 +61,24 @@
                 Assert.Fail($"[DivisionId={x.DivisionId}] Number of team entries does not match.");
             }
 
-            if (x.GameResults.Count == y.GameResults.Count)
+            if (hasComparerByGames)
             {
-                var gameResultComparer = new ShortGameResultDtoComparer();
-                for (var i = 0; i < x.GameResults.Count; i++)
+                if (x.GameResults.Count == y.GameResults.Count)
                 {
-                    if (gameResultComparer.Compare(x.GameResults[i], y.GameResults[i]) != 0)
+                    var gameResultComparer = new ShortGameResultDtoComparer();
+                    for (var i = 0; i < x.GameResults.Count; i++)
                     {
-                        return 1;
+                        if (gameResultComparer.Compare(x.GameResults[i], y.GameResults[i]) != 0)
+                        {
+                            return 1;
+                        }
                     }
                 }
+                else
+                {
+                    Assert.Fail($"[DivisionId={x.DivisionId}] Number of game result entries does not match.");
+                }
             }
-            else
-            {
-                Assert.Fail($"[DivisionId={x.DivisionId}] Number of game result entries does not match.");
-            }
-
             return 0;
         }
     }

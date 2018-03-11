7879752749649109c2212f5549d6a45efc8ba343
Nick Bocharskiy <nickbocharsky@gmail.com>
2018-03-11 09:01:14 +0200

    Recreate project (change target framework to 4.7.1) Try to integrate simple injector(not successful for now).

--- src/VolleyManagement.API/Startup.cs
+++ src/VolleyManagement.API/Startup.cs
@@ -1,35 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Rewrite;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using SimpleInjector.Integration.AspNetCore;
+using SimpleInjector.Integration.AspNetCore.Mvc;
 using VolleyManagement.API.Infrastructure;
-using VolleyManagement.Contracts;
 using VolleyManagement.Crosscutting.Contracts.Infrastructure.IOC;
+using VolleyManagement.Data.MsSql.Infrastructure;
+using VolleyManagement.Services.Infrastructure;
+using VolleyManagement.UI.Infrastructure;
+using VolleyManagement.UI.Infrastructure.IOC;
 using VolleyManagement.Data.Contracts;
+using VolleyManagement.Domain.ContributorsAggregate;
+using VolleyManagement.Domain.FeedbackAggregate;
+using VolleyManagement.Domain.GamesAggregate;
+using VolleyManagement.Domain.PlayersAggregate;
+using VolleyManagement.Domain.RequestsAggregate;
+using VolleyManagement.Domain.RolesAggregate;
+using VolleyManagement.Domain.TeamsAggregate;
+using VolleyManagement.Domain.TournamentRequestAggregate;
+using VolleyManagement.Domain.TournamentsAggregate;
+using VolleyManagement.Domain.UsersAggregate;
 using VolleyManagement.Data.MsSql;
 using VolleyManagement.Data.MsSql.Context;
-using VolleyManagement.Data.MsSql.Infrastructure;
-using VolleyManagement.Data.MsSql.Repositories;
-using VolleyManagement.Domain.TournamentsAggregate;
-using VolleyManagement.Services;
-using VolleyManagement.Services.Infrastructure;
 
 namespace VolleyManagement.API
 {
     public class Startup
     {
-        const string ID_KEY = "GoogleClientId";
-        const string SECRET_KEY = "GoogleClientSecret";
-
+        //private readonly Container container = new Container();
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -41,21 +53,25 @@ namespace VolleyManagement.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
             ConfigureIoc(services);
-            //services.AddDbContext<VolleyManagementEntities>(options =>
-               // options.UseSqlServer(Configuration.GetConnectionString("VolleyManagementEntities")));
-            //var googleId = Configuration[ID_KEY]; ;
-            //var googleSecret = Configuration[SECRET_KEY];
+            // IntegrateSimpleInjector(services);
+        }
 
-            //if (googleId != null && googleSecret != null)
-            //{
-            //    services.AddAuthentication().AddGoogle(googleOptions =>
-            //    {
-            //        googleOptions.ClientId = googleId;
-            //        googleOptions.ClientSecret = googleSecret;
-            //    });
-            //}
+        private void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            // NOTE
+            // Wasn't able to reuse existing IocModules. Additional work required.
+            // Use core Ioc implementation for now.
+
+            //container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            //services.AddSingleton<IControllerActivator>(
+            //    new SimpleInjectorControllerActivator(container));
+
+            //services.EnableSimpleInjectorCrossWiring(container);
+            //services.UseSimpleInjectorAspNetRequestScoping(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,9 +81,6 @@ namespace VolleyManagement.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            //var options = new RewriteOptions().AddRedirectToHttps();
-            //app.UseRewriter(options);
 
             app.UseMvc();
         }

ccd03de82745fafcf3bf90d940e464e98607cda8
YuriiChornyi <yura43528@gmail.com>
2018-03-15 16:11:24 +0200

    :-1:
    suppressed few things, there is explanation on suppress comment
    
    Co-Authored-By: nataliiabidiuk <nataliiabidiuk@users.noreply.github.com>

--- src/VolleyManagement.Backend/VolleyManagement.Domain/Dto/UserInRoleDto.cs
+++ src/VolleyManagement.Backend/VolleyManagement.Domain/Dto/UserInRoleDto.cs
@@ -25,11 +25,11 @@
         /// </summary>
         public string UserName { get; set; }
 
-#pragma warning disable S3956 //Use more generic collection, LINQ convertation fails, EF bug
+#pragma warning disable S3956 // see more https://stackoverflow.com/questions/25528649/exception-on-inner-linq-query-when-calling-tolist?rq=1
         /// <summary>
         /// Gets or sets role Id user belongs to
         /// </summary>
         public List<int> RoleIds { get; set; }
-#pragma warning restore S3956 //see more https://stackoverflow.com/questions/25528649/exception-on-inner-linq-query-when-calling-tolist?rq=1
+#pragma warning restore S3956 // Use more generic collection, LINQ convertation fails, EF bug
     }
 }

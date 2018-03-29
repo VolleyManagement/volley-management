56a07a5c5819a82e6b0dd44fd6a7a5d6f7039c31
Sergii Diachenko <sdiachenko@outlook.com>
2018-03-30 00:56:06 +0300

    Added step stubs

---
+++ tests/VolleyManagement.Specs/TeamsContext/RemoveTeamSteps.cs
@@ -0,0 +1,32 @@
+﻿using TechTalk.SpecFlow;
+
+namespace VolleyManagement.Specs.TeamsContext
+{
+    [Binding]
+    public class RemoveTeamSteps
+    {
+        [Given(@"Volley\.org\.ua team exists")]
+        public void GivenVolley_Org_UaTeamExists()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"football\.org\.ua team does not exist")]
+        public void GivenFootball_Org_UaTeamDoesNotExist()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I execute DeleteTeam")]
+        public void WhenIExecuteDeleteTeam()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"team is removed")]
+        public void ThenTeamIsRemoved()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

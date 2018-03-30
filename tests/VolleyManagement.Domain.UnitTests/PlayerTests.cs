d9b9d2a38e589abe552410d15f8cb7af6305b7ab
Sergii Diachenko <sdiachenko@outlook.com>
2018-03-31 01:09:55 +0300

    Added Domain.UnitTests project

---
+++ tests/VolleyManagement.Domain.UnitTests/PlayerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using VolleyManagement.Domain.PlayersAggregate;
+using Xunit;
+
+namespace VolleyManagement.Domain.UnitTests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void Player_EmptyFirstName_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.FirstName = string.Empty; };
+
+            // Assert
+            act.Should().Throw<ArgumentException>("Empty first name is not allowed.");
+        }
+
+        private static Player CreatePlayer()
+        {
+            return new Player {
+                Id = 1,
+                FirstName = "Ivan",
+                LastName = "Ivanov",
+                Height = 100,
+                Weight = 80,
+                BirthYear = 1988
+            };
+        }
+    }
+}

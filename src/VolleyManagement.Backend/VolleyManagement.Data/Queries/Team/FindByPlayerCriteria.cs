66def854557849ccda90923869fab63230d5ab3c
ViznyakS <viznyak.sofia@ukr.net>
2018-03-30 23:54:27 +0300

    Added FindByIdCriteria, changed GetPlayerTeam and ValidateExistingPlayers

---
+++ src/VolleyManagement.Backend/VolleyManagement.Data/Queries/Team/FindByPlayerCriteria.cs
@@ -0,0 +1,23 @@
+﻿using VolleyManagement.Data.Contracts;
+namespace VolleyManagement.Data.Queries.Team
+{
+    /// <summary>
+    /// Find by player id criteria
+    /// </summary>
+    public class FindByPlayerCriteria : IQueryCriteria
+    {
+        public FindByPlayerCriteria()
+        {
+
+        }
+
+        public FindByPlayerCriteria(int id)
+        {
+            Id = id;
+        }
+        /// <summary>
+        /// Gets or sets Player ID to search for
+        /// </summary>
+        public int Id { get; set; }
+    }
+}

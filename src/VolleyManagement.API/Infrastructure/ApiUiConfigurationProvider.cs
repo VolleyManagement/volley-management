ed189f074ded52fab44d5686f407668fa612c54c
Nick Bocharskiy <nickbocharsky@gmail.com>
2018-04-04 17:21:47 +0300

    Add implementation for ISecretsProvider interface into ApiUiConfigurationClass

--- src/VolleyManagement.API/Infrastructure/ApiUiConfigurationProvider.cs
+++ src/VolleyManagement.API/Infrastructure/ApiUiConfigurationProvider.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using VolleyManagement.Crosscutting.Contracts.Providers;
 using IConfigurationProvider = VolleyManagement.Crosscutting.Contracts.Providers.IConfigurationProvider;
 
 namespace VolleyManagement.API.Infrastructure
 {
-    public class ApiUiConfigurationProvider : IConfigurationProvider
+    public class ApiUiConfigurationProvider : IConfigurationProvider, ISecretsProvider
     {
         public string GetVolleyManagementEntitiesConnectionString()
         {
             var builder = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appsettings.json", true).Build();
             return builder.GetConnectionString("VolleyManagementEntities");
+        }
+
+        public IConfigurationSection GetGoogleClientId()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Path.Combine(Environment.CurrentDirectory,"../../..")).AddJsonFile("secrets.json", true).Build();
+            return builder.GetSection("Authentication:GoogleClientId");
         }
     }
 }

57f94c93a555fdfc4a4503bc97454430a1e0dcb3
Nick Bocharskiy <nickbocharsky@gmail.com>
2018-04-04 17:28:07 +0300

    Add new classes for user info provided by google auth and google client secrets

---
+++ src/VolleyManagement.API/Helpers/GoogleSecrets.cs
@@ -0,0 +1,18 @@
+﻿namespace VolleyManagement.API.Helpers
+{
+    /// <summary>
+    /// Contains client id and client secret for goole auth
+    /// </summary>
+    public class GoogleSecrets
+    {
+        /// <summary>
+        /// Gets or sets client id for google auth
+        /// </summary>
+        public string GoogleClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets client secret for google auth
+        /// </summary>
+        public string GoogleClientSecret { get; set; }
+    }
+}
---
+++ src/VolleyManagement.API/Model/GoogleUserInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace VolleyManagement.API.Model
+{
+    /// <summary>
+    /// Contains user info provided by google auth
+    /// </summary>
+    public class GoogleUserInfo
+    {
+        public string AccessToken { get; set; }
+
+        public string ExpiresAt { get; set; }
+
+        public string ExpiresIn { get; set; }
+
+        public string IdToken { get; set; }
+
+        public string Code { get; set; }
+
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string Email { get; set; }
+    }
+}

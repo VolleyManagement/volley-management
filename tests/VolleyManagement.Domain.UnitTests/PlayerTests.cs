a8d6a32001ef6797d856fae61c9f7a476797fb5e
andriipirko <36235893+andriipirko@users.noreply.github.com>
2018-04-05 01:56:34 +0300

    Add Player entity tests. (#308)

--- tests/VolleyManagement.Domain.UnitTests/PlayerTests.cs
+++ tests/VolleyManagement.Domain.UnitTests/PlayerTests.cs
@@ -7,6 +7,67 @@ namespace VolleyManagement.Domain.UnitTests
 {
     public class PlayerTests
     {
+        private const int TEST_PLAYER_ID = 1;
+        private const string TEST_PLAYER_FIRST_NAME = "Ivan";
+        private const string TEST_PLAYER_LAST_NAME = "Ivanov";
+        private const short TEST_PLAYER_BIRTH_YEAR = 1988;
+        private const short TEST_PLAYER_HEIGHT = 100;
+        private const short TEST_PLAYER_WEIGHT = 80;
+
+        [Fact]
+        public void Player_CreateValidInstance_PlayerCreated()
+        {
+            // Arrange
+            Player player;
+
+            // Act
+            player = CreatePlayer();
+
+            // Assert
+            AssertCorrectPlayerCreated(player);
+        }
+
+        [Fact]
+        public void Player_BirthDateIsNull_ExceptionIsNotThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.BirthYear = null; };
+
+            // Assert
+            act.Should().NotThrow<ArgumentException>("Player's birth date can be null.");
+        }
+
+        [Fact]
+        public void Player_HeightIsNull_ExceptionIsNotThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.Height = null; };
+
+            // Assert
+            act.Should().NotThrow<ArgumentException>("Player's height can be null.");
+        }
+
+        [Fact]
+        public void Player_WeightIsNull_ExceptionIsNotThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.Weight = null; };
+
+            // Assert
+            act.Should().NotThrow<ArgumentException>("Player's weight can be null.");
+        }
+
+        #region FirstName
+
         [Fact]
         public void Player_EmptyFirstName_ExceptionIsThrown()
         {
@@ -20,16 +81,194 @@ namespace VolleyManagement.Domain.UnitTests
             act.Should().Throw<ArgumentException>("Empty first name is not allowed.");
         }
 
+        [Fact]
+        public void Player_FirstNameContainsNumber_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.FirstName = "Player1"; };
+
+            // Assert
+            act.Should().Throw<ArgumentException>("First name cannot contain number.");
+        }
+
+        [Fact]
+        public void Player_FirstNameLengthGreaterThanMaximumAllowedValue_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.FirstName = GeneratePlayerName(Constants.Player.MAX_FIRST_NAME_LENGTH + 1); };
+
+            // Assert
+            act.Should().Throw<ArgumentException>("First name length should not be longer than maximum allowed value.");
+        }
+
+        #endregion
+
+        #region LastName
+
+        [Fact]
+        public void Player_EmptyLastName_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.LastName = string.Empty; };
+
+            // Assert
+            act.Should().Throw<ArgumentException>("Empty last name is not allowed.");
+        }
+
+        [Fact]
+        public void Player_LastNameContainsNumber_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.LastName = "Player1"; };
+
+            // Assert
+            act.Should().Throw<ArgumentException>("Last name cannot contain number.");
+        }
+
+        [Fact]
+        public void Player_LastNameLengthGreaterThanMaximumAllowedValue_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.FirstName = GeneratePlayerName(Constants.Player.MAX_LAST_NAME_LENGTH + 1); };
+
+            // Assert
+            act.Should().Throw<ArgumentException>("Last name length should not be longer than maximum allowed value.");
+        }
+
+        #endregion
+
+        #region BirthYear
+
+        [Fact]
+        public void Player_BirthDateIsLessThanMinimalAllowedValue_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.BirthYear = (Constants.Player.MIN_BIRTH_YEAR - 1); };
+
+            // Arrange
+            act.Should().Throw<ArgumentException>("Player's birth year should not be less than minimum allowed value.");
+        }
+
+        [Fact]
+        public void Player_BirthDateIsGreaterThanMaximumAllowedValue_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.BirthYear = Constants.Player.MAX_BIRTH_YEAR + 1; };
+
+            // Arrange
+            act.Should().Throw<ArgumentException>("Player's birth year should not be greater than maximum allowed value.");
+        }
+
+        #endregion
+
+        #region Height
+
+        [Fact]
+        public void Player_HeightIsLessThanMinimalAllowedValue_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.Height = (Constants.Player.MIN_HEIGHT - 1); };
+
+            // Arrange
+            act.Should().Throw<ArgumentException>("Player's height should not be less than minimum allowed value.");
+        }
+
+        [Fact]
+        public void Player_HeightIsGreaterThanMaximumAllowableValue_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.Height = Constants.Player.MAX_HEIGHT + 1; };
+
+            // Arrange
+            act.Should().Throw<ArgumentException>("Player's height should not be greater than maximum allowed value.");
+        }
+
+        #endregion
+
+        #region Weight
+
+        [Fact]
+        public void Player_WeightIsLessThanMinimalAllowableValue_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.Weight = (Constants.Player.MIN_WEIGHT - 1); };
+
+            // Arrange
+            act.Should().Throw<ArgumentException>("Player's weight should not be less than minimum allowed value.");
+        }
+
+        [Fact]
+        public void Player_WeightIsGreaterThanMaximumAllowableValue_ExceptionIsThrown()
+        {
+            // Arrange
+            var player = CreatePlayer();
+
+            // Act
+            Action act = () => { player.Height = Constants.Player.MAX_WEIGHT + 1; };
+
+            // Arrange
+            act.Should().Throw<ArgumentException>("Player's weight should not be greater than maximum allowed value.");
+        }
+
+        #endregion
+
+        #region Private
+
         private static Player CreatePlayer()
         {
-            return new Player {
-                Id = 1,
-                FirstName = "Ivan",
-                LastName = "Ivanov",
-                Height = 100,
-                Weight = 80,
-                BirthYear = 1988
-            };
+            return new Player(TEST_PLAYER_ID,
+                              TEST_PLAYER_FIRST_NAME,
+                              TEST_PLAYER_LAST_NAME,
+                              TEST_PLAYER_BIRTH_YEAR,
+                              TEST_PLAYER_HEIGHT,
+                              TEST_PLAYER_WEIGHT);
         }
+
+        private string GeneratePlayerName(int length)
+        {
+            return string.Concat(System.Linq.Enumerable.Repeat("a", length));
+        }
+
+        private void AssertCorrectPlayerCreated(Player actual)
+        {
+            actual.Id.Should().Be(TEST_PLAYER_ID, "Player's id wasn't set properly.");
+            actual.FirstName.Should().Be(TEST_PLAYER_FIRST_NAME, "Player's first name wasn't set properly.");
+            actual.LastName.Should().Be(TEST_PLAYER_LAST_NAME, "Player's last name wasn't set properly.");
+            actual.BirthYear.Should().Be(TEST_PLAYER_BIRTH_YEAR, "Player's birth year wasn't set properly.");
+            actual.Height.Should().Be(TEST_PLAYER_HEIGHT, "Player's height wasn't set properly.");
+            actual.Weight.Should().Be(TEST_PLAYER_WEIGHT, "Player's weight wasn't set properly.");
+        }
+
+        #endregion
     }
 }

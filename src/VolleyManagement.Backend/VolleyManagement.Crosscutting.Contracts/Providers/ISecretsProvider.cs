3040131180ef29de0718ab56d8acd811b6dfdc62
Nick Bocharskiy <nickbocharsky@gmail.com>
2018-04-04 17:20:46 +0300

    Add interface ISecretProvider

---
+++ src/VolleyManagement.Backend/VolleyManagement.Crosscutting.Contracts/Providers/ISecretsProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace VolleyManagement.Crosscutting.Contracts.Providers
+{
+    /// <summary>
+    /// Provides configurations for external auth secrets
+    /// </summary>
+    public interface ISecretsProvider
+    {
+        IConfigurationSection GetGoogleClientId();
+    }
+}

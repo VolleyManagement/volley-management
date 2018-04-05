caee88bbc83322b296cd157a3a2fae2fc61cf337
Myroslavv <myroslav.vyspyansky@gmail.com>
2018-04-05 16:48:31 +0300

    Update ITeamRepo.Add signature.

--- src/VolleyManagement.Backend/VolleyManagement.Domain/TeamsAggregate/ITeamRepository.cs
+++ src/VolleyManagement.Backend/VolleyManagement.Domain/TeamsAggregate/ITeamRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace VolleyManagement.Domain.TeamsAggregate
 {
     using Data.Contracts;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Defines specific contract for PlayerRepository
@@ -8,10 +9,15 @@
     public interface ITeamRepository
     {
         /// <summary>
-        /// Adds new team.
+        /// Adds new team to database.
         /// </summary>
-        /// <param name="newEntity">The team for adding.</param>
-        Team Add(Team newEntity);
+        /// <param name="name">Name of the team</param>
+        /// <param name="coach">Team's coach</param>
+        /// <param name="captain">Captain's id</param>
+        /// <param name="achievements">Achievements</param>
+        /// <param name="roster">Collection of id of team's members.</param>
+        /// <returns>Team representation of entity.</returns>
+        Team Add(string name, string coach, PlayerId captain, string achievements, ICollection<PlayerId> roster);
 
         /// <summary>
         /// Updates specified team.

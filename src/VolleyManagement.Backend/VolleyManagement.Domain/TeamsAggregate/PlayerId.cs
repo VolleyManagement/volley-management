43914e7739e1d9e7dc905cb9d44e89f6e7df09db
Myroslavv <myroslav.vyspyansky@gmail.com>
2018-04-06 15:25:23 +0300

    Make PlayerId immutable.

--- src/VolleyManagement.Backend/VolleyManagement.Domain/TeamsAggregate/PlayerId.cs
+++ src/VolleyManagement.Backend/VolleyManagement.Domain/TeamsAggregate/PlayerId.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VolleyManagement.Domain.TeamsAggregate
+﻿namespace VolleyManagement.Domain.TeamsAggregate
 {
     public class PlayerId
     {
-        public int Id { get; set; }
+        public int Id { get; }
+        public PlayerId(int id) { Id = id; }
     }
 }

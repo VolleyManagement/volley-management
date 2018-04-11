2d90ae77085e1977f8a0eb806ef047b6917eea43
Yurii Chornyi <yura43528@gmail.com>
2018-04-11 10:00:49 +0300

    changed property to avoid null check

--- src/VolleyManagement.Backend/VolleyManagement.UI/Areas/Mvc/ViewModels/Teams/TeamViewModel.cs
+++ src/VolleyManagement.Backend/VolleyManagement.UI/Areas/Mvc/ViewModels/Teams/TeamViewModel.cs
@@ -69,7 +69,7 @@
         /// Gets or sets the roster of the team
         /// </summary>
         [Display(Name = "TeamRoster", ResourceType = typeof(ViewModelResources))]
-        public ICollection<PlayerNameViewModel> Roster { get; set; }
+        public ICollection<PlayerNameViewModel> Roster { get; set; } = new List<PlayerNameViewModel>();
 
         /// <summary>
         /// Gets or sets the photo of the team
@@ -122,8 +122,7 @@
                 Coach,
                 Achievements,
                 new PlayerId(Captain.Id),
-                Roster == null ? new List<PlayerId>() :
-                    Roster.Select(x => new PlayerId(x.Id)));
+                Roster.Select(x => new PlayerId(x.Id)));
         }
 
         /// <summary>

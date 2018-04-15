3c4a43fb8ad6dd072410f91a70047ea82608cc71
Nick Bocharskiy <nickbocharsky@gmail.com>
2018-04-15 18:12:25 +0300

    Fix sonarqube error

--- src/VolleyManagement.API/Program.cs
+++ src/VolleyManagement.API/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace VolleyManagement.API
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {

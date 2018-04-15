ba9d0dd40c2fcfd7dc2564ede97f70c3741f875f
Nick Bocharskiy <nickbocharsky@gmail.com>
2018-04-15 18:20:49 +0300

    Fix sonarqube errors.

--- src/VolleyManagement.API/Startup.cs
+++ src/VolleyManagement.API/Startup.cs
@@ -37,7 +37,7 @@ namespace VolleyManagement.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
             services.AddMvc();
@@ -52,7 +52,7 @@ namespace VolleyManagement.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public static void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
 
@@ -124,7 +124,7 @@ namespace VolleyManagement.API
             };
         }
 
-        private void InitializeConnectionString()
+        private static void InitializeConnectionString()
         {
             VolleyManagementDbContextFactory.ConnectionNameOrString = GetVolleyManagementEntitiesConnectionString();
         }

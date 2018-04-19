79c2f5b9c9691e89f8f00df606df4f25a6a19da0
Nick Bocharskiy <nickbocharsky@gmail.com>
2018-04-19 19:03:18 +0300

    Add comment. Remove code which was used during development.

--- src/VolleyManagement.WebClient/spa/app/Services/tokenInterceptor.ts
+++ src/VolleyManagement.WebClient/spa/app/Services/tokenInterceptor.ts
@@ -5,17 +5,16 @@ import 'rxjs/add/operator/do';
 
 @Injectable()
 // Intercepts an outgoing HttpRequest and optionally transform it or the response.
-// Used to add 'withCredentials: true' to every request
+// Used to add 'withCredentials: true' to every request.
+// withCredentials property is a Boolean that indicates whether or not cross-site
+// Access-Control requests should be made using credentials such as cookies,
+// authorization headers or TLS client certificates.
 export class TokenInterceptor implements HttpInterceptor {
 
     constructor() { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         request = request.clone({ withCredentials: true });
-        return next.handle(request).do(event => {
-            if (event instanceof HttpResponse) {
-                console.log(event);
-            }
-        });
+        return next.handle(request);
     }
 }

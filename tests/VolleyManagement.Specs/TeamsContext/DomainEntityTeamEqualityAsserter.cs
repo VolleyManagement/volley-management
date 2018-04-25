6d92bfb6c04d4f3b6a8f870467160eb315d3ebc4
Myroslavv <vmv2580@gmail.com>
2018-04-25 16:20:46 +0300

    Create class to compare Team Data in, at least, 3 features - Create, Edit and ManagePlayers.

---
+++ tests/VolleyManagement.Specs/TeamsContext/DomainEntityTeamEqualityAsserter.cs
@@ -0,0 +1,33 @@
+﻿namespace VolleyManagement.Specs.TeamsContext
+{
+    using FluentAssertions;
+    using System.Collections.Generic;
+    using VolleyManagement.Data.MsSql.Entities;
+    using VolleyManagement.Domain.TeamsAggregate;
+
+    using static System.Linq.Enumerable;
+
+    /// <summary>
+    /// Checks if domain team equals team entity.
+    /// </summary>
+    static class DomainEntityTeamEqualityAsserter
+    {
+        public static void AssertSimpleDataIsEqual(TeamEntity entity, Team domain)
+        {
+            entity.Id.Should().Be(domain.Id);
+            entity.Name.Should().Be(domain.Name);
+            entity.Coach.Should().Be(domain.Coach);
+            entity.Achievements.Should().Be(domain.Achievements);
+            entity.CaptainId.Should().Be(domain.Captain.Id);
+        }
+
+        public static void AssertRosterIsEqual(ICollection<PlayerEntity> entity, ICollection<PlayerId> domain)
+        {
+            entity.Count.Should().Be(domain.Count, "Roster's count must be equal.");
+
+            var entityRosterIds = entity.Select(e => e.Id);
+            var domainRosterIds = domain.Select(d => d.Id);
+            entityRosterIds.Should().BeEquivalentTo(domainRosterIds);
+        }
+    }
+}

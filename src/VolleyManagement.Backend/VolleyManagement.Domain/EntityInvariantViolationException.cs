cba9aad99e9340ae9d40040911e82a93768af62b
Rom Ben <rom.ben.ten@gmail.com>
2018-05-02 13:30:29 +0300

    Add exeption

--- src/VolleyManagement.Backend/VolleyManagement.Domain/EntityInvariantViolationException.cs
+++ src/VolleyManagement.Backend/VolleyManagement.Domain/EntityInvariantViolationException.cs
@@ -15,36 +15,5 @@
             : base("Entity Invariant Violation")
         {
         }
-
-        ///// <summary>
-        ///// Initializes a new instance of the <see cref="EntityInvariantViolationException"/> class.
-        ///// </summary>
-        ///// <param name="message">The message.</param>
-        ///// <param name="entityId">Id related to error occur</param>
-        //public EntityInvariantViolationException(int? entityId)
-        //    : base($"Team with id {entityId} is not valid ")
-        //{
-        //}
-
-        ///// <summary>
-        ///// Initializes a new instance of the <see cref="EntityInvariantViolationException"/> class.
-        ///// </summary>
-        ///// <param name="message">The message.</param>
-        ///// <param name="entityName">Id related to error occur</param>
-        //public EntityInvariantViolationException(string entityName)
-        //    : base($"Team with name {entityName} is not valid ")
-        //{
-        //}
-
-        ///// <summary>
-        ///// Initializes a new instance of the <see cref="EntityInvariantViolationException"/> class.
-        ///// </summary>
-        ///// <param name="message">The message.</param>
-        ///// <param name="entityName">Id related to error occur</param>
-        //public EntityInvariantViolationException(string entityCoachName, int a)
-        //    : base($"Team with name {entityCoachName} is not valid ")
-        //{
-        //}
-
 }
 }

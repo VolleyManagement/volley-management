0196d4247d7f162a0da88b0ee20f22b1e5dfd890
Rom Ben <rom.ben.ten@gmail.com>
2018-05-02 13:28:08 +0300

    Add exeption

---
+++ src/VolleyManagement.Backend/VolleyManagement.Domain/EntityInvariantViolationException.cs
@@ -0,0 +1,50 @@
+﻿namespace VolleyManagement.Domain.TeamsAggregate
+{
+    using System;
+
+    /// <summary>
+    /// Contains information about thrown authorization exception
+    /// </summary>
+    [Serializable]
+    public class EntityInvariantViolationException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityInvariantViolationException"/> class
+        /// </summary>
+        public EntityInvariantViolationException()
+            : base("Entity Invariant Violation")
+        {
+        }
+
+        ///// <summary>
+        ///// Initializes a new instance of the <see cref="EntityInvariantViolationException"/> class.
+        ///// </summary>
+        ///// <param name="message">The message.</param>
+        ///// <param name="entityId">Id related to error occur</param>
+        //public EntityInvariantViolationException(int? entityId)
+        //    : base($"Team with id {entityId} is not valid ")
+        //{
+        //}
+
+        ///// <summary>
+        ///// Initializes a new instance of the <see cref="EntityInvariantViolationException"/> class.
+        ///// </summary>
+        ///// <param name="message">The message.</param>
+        ///// <param name="entityName">Id related to error occur</param>
+        //public EntityInvariantViolationException(string entityName)
+        //    : base($"Team with name {entityName} is not valid ")
+        //{
+        //}
+
+        ///// <summary>
+        ///// Initializes a new instance of the <see cref="EntityInvariantViolationException"/> class.
+        ///// </summary>
+        ///// <param name="message">The message.</param>
+        ///// <param name="entityName">Id related to error occur</param>
+        //public EntityInvariantViolationException(string entityCoachName, int a)
+        //    : base($"Team with name {entityCoachName} is not valid ")
+        //{
+        //}
+
+}
+}

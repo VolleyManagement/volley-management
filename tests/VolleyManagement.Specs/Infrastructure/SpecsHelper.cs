ac33839e2e13af9e52e92b9cb9f61f921567d388
ViznyakS <viznyak.sofia@ukr.net>
2018-05-14 12:02:39 +0300

    Modify SpecHelper

--- tests/VolleyManagement.Specs/Infrastructure/SpecsHelper.cs
+++ tests/VolleyManagement.Specs/Infrastructure/SpecsHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VolleyManagement.Specs.Infrastructure
+﻿namespace VolleyManagement.Specs.Infrastructure
 {
     /// <summary>
     /// Provides help methods for spec tests
@@ -18,7 +12,7 @@ namespace VolleyManagement.Specs.Infrastructure
         /// <returns></returns>
         public static (string FirstName, string LastName) SplitFullNameToFirstLastNames(string fullName)
         {
-            var whitespaceCharIndex = fullName.IndexOf(' ');
+            var whitespaceCharIndex = fullName.LastIndexOf(' ');
             var firstName = fullName.Substring(0, whitespaceCharIndex);
             var lastName = fullName.Substring(whitespaceCharIndex + 1);
 

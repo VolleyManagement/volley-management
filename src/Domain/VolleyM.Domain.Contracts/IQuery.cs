f6fb858c239c366ced33325faeffa36aee0253a1
Sergii Diachenko <sdiachenko@outlook.com>
2019-08-24 01:45:45 +0300

    Added Domain Contracts

---
+++ src/Domain/VolleyM.Domain.Contracts/IQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VolleyM.Domain.Contracts
+{
+    /// <summary>
+    /// Represents Query object to be executed against persistence layer
+    /// </summary>
+    /// <typeparam name="TParam">Query parameters</typeparam>
+    /// <typeparam name="TResult">Query result</typeparam>
+    public interface IQuery<in TParam, out TResult>
+        where TParam : notnull
+    {
+        TResult Execute(TParam param);
+    }
+}
---
+++ src/Domain/VolleyM.Domain.Contributors.Contracts/ContributorDto.cs
@@ -0,0 +1,11 @@
+﻿namespace VolleyM.Domain.Contributors.Contracts
+{
+    public class ContributorDto
+    {
+        public string FullName { get; set; }
+
+        public string Team { get; set; }
+
+        public string CourseDirection { get; set; }
+    }
+}

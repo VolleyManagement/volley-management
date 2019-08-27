72ae1512296319fa2caa3f217a4a90f4493a9731
Sergii Diachenko <sdiachenko@outlook.com>
2019-08-27 17:10:47 +0300

    moved VolleyManagement.API -> Client/VolleyM.API

--- src/Client/VolleyM.API/Program.cs
+++ src/Client/VolleyM.API/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using VolleyM.Infrastructure.Bootstrap;
 
-namespace VolleyManagement.API
+namespace VolleyM.API
 {
     public class Program
     {

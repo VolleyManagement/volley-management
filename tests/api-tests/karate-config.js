bb698d35cf719666a43fc4cfeb8db7d71af6e768
Sergii Diachenko <sdiachenko@outlook.com>
2019-09-02 08:45:35 +0300

    Added simple karate test

---
+++ tests/api-tests/karate-config.js
@@ -0,0 +1,19 @@
+function fn() {
+  var env = karate.env; // get java system property 'karate.env'
+  karate.log('karate.env system property is set:', env);
+  if (!env) {
+    env = 'dev'; // a custom 'intelligent' default
+  }
+  var config = { // base config JSON
+    vmAppUrl: 'http://localhost:5000'
+  };
+
+  if (env === 'staging') {
+    config.vmAppUrl = 'https://staging-volley-mgmt.azurewebsites.net/';
+  }
+
+  // don't waste time waiting for a connection or if servers don't respond within 5 seconds
+  karate.configure('connectTimeout', 5000);
+  karate.configure('readTimeout', 5000);
+  return config;
+}

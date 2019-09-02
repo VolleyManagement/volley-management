5b27485b6e89703782461c3a5b4834536a3f98f6
Sergii Diachenko <sdiachenko@outlook.com>
2019-09-02 15:16:01 +0300

    Removed cert check

--- tests/api-tests/get-karate.ps1
+++ tests/api-tests/get-karate.ps1
@@ -2,4 +2,4 @@
 
 $url = "https://dl.bintray.com/ptrthomas/karate/karate-0.9.4.jar";
 
-Invoke-WebRequest -Uri $url -OutFile .\karate.jar -Headers @{'Host' = 'dl.bintray.com'; 'Referer' = 'https://dl.bintray.com/ptrthomas/karate/'} -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+Invoke-WebRequest -Uri $url -OutFile .\karate.jar -SkipCertificateCheck -Headers @{'Host' = 'dl.bintray.com'; 'Referer' = 'https://dl.bintray.com/ptrthomas/karate/'} -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'

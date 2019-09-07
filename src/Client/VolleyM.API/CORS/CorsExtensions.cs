c4e27b82ef3a8b81e177aa4103e167e2fe2c8613
Sergii Diachenko <sdiachen@users.noreply.github.com>
2019-09-07 09:14:10 +0300

    Added CORS (#424)

---
+++ src/Client/VolleyM.API/CORS/CorsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VolleyM.API.CORS
+{
+    public static class CorsExtensions
+    {
+        public static void AddCorsFromSettings(this IServiceCollection services, CorsOptions corsOpts)
+        {
+            var configuredOrigins = ParseOrigins(corsOpts.AllowedOrigins);
+            services.AddCors(opts =>
+            {
+                opts.AddDefaultPolicy(pol =>
+                {
+                    pol.WithOrigins(configuredOrigins)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+        }
+
+        private static string[] ParseOrigins(string allowedOriginsStr)
+            => allowedOriginsStr.Split(';', StringSplitOptions.RemoveEmptyEntries);
+    }
+}

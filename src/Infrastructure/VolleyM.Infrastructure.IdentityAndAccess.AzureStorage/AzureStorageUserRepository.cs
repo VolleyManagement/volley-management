3a9cafe72f774c61e75912ad9d8b4933606aeaf4
Sergii Diachenko <sdiachenko@outlook.com>
2019-10-04 11:13:08 +0300

    Added new project

---
+++ src/Infrastructure/VolleyM.Infrastructure.IdentityAndAccess.AzureStorage/AzureStorageUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using VolleyM.Domain.Contracts;
+using VolleyM.Domain.IdentityAndAccess;
+
+namespace VolleyM.Infrastructure.IdentityAndAccess.AzureStorage
+{
+    public class AzureStorageUserRepository : IUserRepository
+    {
+        public Task<Unit> Add(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Result<User>> Get(UserId id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Infrastructure/VolleyM.Infrastructure.IdentityAndAccess.AzureStorage/InfrastructureIdentityAndAccessAzureStorageAssemblyBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper.Configuration;
+using SimpleInjector;
+using VolleyM.Domain.IdentityAndAccess;
+using VolleyM.Infrastructure.Bootstrap;
+
+namespace VolleyM.Infrastructure.IdentityAndAccess.AzureStorage
+{
+    public class InfrastructureIdentityAndAccessAzureStorageAssemblyBootstrapper : IAssemblyBootstrapper
+    {
+        public void RegisterDependencies(Container container)
+        {
+            container.Register<IUserRepository, AzureStorageUserRepository>(Lifestyle.Scoped);
+        }
+
+        public void RegisterMappingProfiles(MapperConfigurationExpression mce)
+        {
+        }
+    }
+}

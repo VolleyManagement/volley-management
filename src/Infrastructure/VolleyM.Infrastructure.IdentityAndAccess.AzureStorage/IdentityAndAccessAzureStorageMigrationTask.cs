c09a451e82556e4586223bde6aeb0c255cd82f32
Sergii Diachenko <sdiachenko@outlook.com>
2019-10-07 16:35:58 +0300

    Implemented migration

--- src/Infrastructure/VolleyM.Infrastructure.IdentityAndAccess.AzureStorage/IdentityAndAccessAzureStorageMigrationTask.cs
+++ src/Infrastructure/VolleyM.Infrastructure.IdentityAndAccess.AzureStorage/IdentityAndAccessAzureStorageMigrationTask.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using VolleyM.Infrastructure.IdentityAndAccess.AzureStorage.TableConfiguration;
 using VolleyM.Tools.AzureStorageMigrator.Contracts;
 
 namespace VolleyM.Infrastructure.IdentityAndAccess.AzureStorage
@@ -9,16 +10,24 @@ namespace VolleyM.Infrastructure.IdentityAndAccess.AzureStorage
     [Export(typeof(IMigrationTask))]
     public class IdentityAndAccessAzureStorageMigrationTask : IMigrationTask
     {
+        private IdentityContextTableStorageOptions _options;
+        private TableConfiguration.TableConfiguration _tableConfig;
+
         public Task Initialize(IConfiguration config)
         {
+            _options = config.GetSection("IdentityContextTableStorageOptions")
+                .Get<IdentityContextTableStorageOptions>();
+
+            _tableConfig = new TableConfiguration.TableConfiguration(_options);
+
             Log.Information("IaAContext initialization complete.");
             return Task.CompletedTask;
         }
 
-        public Task MigrateUp()
+        public async Task MigrateUp()
         {
+            await _tableConfig.ConfigureTables();
             Log.Information("IaAContext migration complete.");
-            return Task.CompletedTask;
         }
     }
 }

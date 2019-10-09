3bda861835e631e1979480594cd4463fdcfa88db
Sergii Diachenko <sdiachenko@outlook.com>
2019-10-09 09:43:25 +0300

    Basic AuthorizationHandler wrapper

---
+++ src/Client/VolleyM.API/Authorization/AuthorizationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleInjector;
+
+namespace VolleyM.API.Authorization
+{
+    public static class AuthorizationExtensions
+    {
+        public static IServiceCollection AddDefaultVolleyMAuthorization(this IServiceCollection services, Container container)
+        {
+            services.AddAuthorizationCore(config =>
+            {
+                config.DefaultPolicy = new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .AddRequirements(new DefaultVolleyMAuthorizationRequirement())
+                    .Build();
+            });
+
+            services.AddScoped<IAuthorizationHandler>(service => new VolleyMDefaultAuthorizationHandler(container));
+
+            return services;
+        }
+    }
+}
---
+++ src/Client/VolleyM.API/Authorization/DefaultVolleyMAuthorizationRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace VolleyM.API.Authorization
+{
+    public class DefaultVolleyMAuthorizationRequirement : IAuthorizationRequirement
+    {
+    }
+}

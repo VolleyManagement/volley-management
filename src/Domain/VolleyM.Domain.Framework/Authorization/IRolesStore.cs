d7a9b00bcee14b3a98db17bbad67de09092cb02e
Sergii Diachenko <sdiachenko@outlook.com>
2019-10-12 19:31:45 +0300

    Added Roles store hardcoded

---
+++ src/Domain/VolleyM.Domain.Framework/Authorization/IRolesStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using VolleyM.Domain.Contracts;
+using VolleyM.Domain.IdentityAndAccess.RolesAggregate;
+
+namespace VolleyM.Domain.Framework.Authorization
+{
+    public interface IRolesStore
+    {
+        Task<Result<Role>> Get(RoleId roleId);
+    }
+}

4a9943ac99599c0033ccea77e8742c1aa7db808b
Sergii Diachenko <sdiachenko@outlook.com>
2019-10-12 19:31:45 +0300

    Added Roles store hardcoded

---
+++ src/Infrastructure/VolleyM.Infrastructure.Hardcoded/HardcodedRolesStore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using VolleyM.Domain.Contracts;
+using VolleyM.Domain.Framework.Authorization;
+using VolleyM.Domain.IdentityAndAccess.RolesAggregate;
+
+namespace VolleyM.Infrastructure.Hardcoded
+{
+    public class HardcodedRolesStore : IRolesStore
+    {
+        private static readonly RoleId _visitor = new RoleId("visitor");
+        private readonly Dictionary<RoleId, Role> _roles = new Dictionary<RoleId, Role>
+        {
+            [_visitor] = new Role(_visitor)
+        };
+
+        public HardcodedRolesStore()
+        {
+            var visitor = new Role(_visitor);
+            visitor.AddPermission(new Permission("contributors", "getall"));
+        }
+
+        public Task<Result<Role>> Get(RoleId roleId)
+        {
+            if (_roles.TryGetValue(roleId, out var role))
+            {
+                return Task.FromResult<Result<Role>>(role);
+            }
+            return Task.FromResult<Result<Role>>(Error.NotFound());
+        }
+    }
+}

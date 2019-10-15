281987fe73300c53f56a004cf3a2ae4d9d3dd62f
YarishPetro <YarishP17@gmail.com>
2019-10-15 17:35:26 +0300

    Player domain is implemented

---
+++ src/Domain/VolleyM.Domain.Players/Handlers/GetAllPlayers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VolleyM.Domain.Contracts;
+
+namespace VolleyM.Domain.Players.Handlers
+{
+    using IQueryObject = IQuery<TenantId, List<PlayerDto>>;
+
+    public class GetAllPlayers
+    {
+        public class Request : IRequest<List<PlayerDto>>
+        {
+            // no params
+        }
+        public class Handler : IRequestHandler<Request, List<PlayerDto>>
+        {
+            private readonly IQueryObject _query;
+
+            public Handler(IQueryObject query)
+            {
+                _query = query;
+            }
+
+            public Task<Result<List<PlayerDto>>> Handle(Request request)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/Domain/VolleyM.Domain.Players/PlayerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VolleyM.Domain.Players
+{
+    public class PlayerDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string BirthYear { get; set; }
+
+        public string Height { get; set; }
+
+        public string Weight { get; set; }
+    }
+}

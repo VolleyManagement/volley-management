8b5e707aadcf73572b11dd65d6dcf990460fffff
Sergii Diachenko <sdiachenko@outlook.com>
2019-10-18 08:51:03 +0300

    Implemented basic decorator

---
+++ src/Domain/VolleyM.Domain.Framework/DecoratorBase.cs
@@ -0,0 +1,33 @@
+﻿namespace VolleyM.Domain.Framework
+{
+    public abstract class DecoratorBase<T>
+    {
+        /// <summary>
+        /// Instance wrapped by decorator, might be another decorator
+        /// </summary>
+        protected T Decoratee { get; }
+
+        /// <summary>
+        /// Original non-decorator instance wrapped
+        /// </summary>
+        protected T RootInstance
+        {
+            get
+            {
+                var result = Decoratee;
+
+                while (result is DecoratorBase<T> wrapped)
+                {
+                    result = wrapped.Decoratee;
+                }
+
+                return result;
+            }
+        }
+
+        protected DecoratorBase(T decoratee)
+        {
+            Decoratee = decoratee;
+        }
+    }
+}

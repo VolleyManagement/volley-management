50fb08ff2c227437c6a291d63df22819a82ba065
Sergii Diachenko <sdiachenko@outlook.com>
2019-10-20 08:04:56 +0300

    Fixed Contributors tests

--- tests/unit-tests/VolleyM.Domain.Contributors.UnitTests/ContributorsStepsBase.cs
+++ tests/unit-tests/VolleyM.Domain.Contributors.UnitTests/ContributorsStepsBase.cs
@@ -1,25 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using BoDi;
 using TechTalk.SpecFlow;
 using VolleyM.Domain.UnitTests.Framework;
 using VolleyM.Infrastructure.Bootstrap;
 
 namespace VolleyM.Domain.Contributors.UnitTests
 {
-    public class ContributorsStepsBase : SpecFlowBindingBase
+    [Binding]
+    public class ContributorsStepsBase : DomainStepsBase
     {
-        [BeforeTestRun]
-        public static void OneTimeSetup()
+        public ContributorsStepsBase(IObjectContainer objectContainer) : base(objectContainer)
         {
-            BeforeTestRun();
+        }
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            TestRunFixtureBase.BeforeTestRun();
         }
 
         [AfterTestRun]
-        public static void OneTimeTearDown()
+        public static void AfterTestRun()
         {
-            AfterTestRun();
+            TestRunFixtureBase.AfterTestRun();
+        }
+        protected override ITestFixture CreateTestFixture(TestTarget target)
+        {
+            return target switch
+            {
+                TestTarget.Unit => new UnitContributorsTestFixture(),
+                _ => throw new NotSupportedException()
+            };
         }
 
-        protected override IEnumerable<IAssemblyBootstrapper> GetAssemblyBootstrappers() =>
+        protected override bool RequiresAuthorizationFixture => true;
+
+        protected override Type GetConcreteTestFixtureType => typeof(IContributorsTestFixture);
+
+        protected override IEnumerable<IAssemblyBootstrapper> GetAssemblyBootstrappers(TestTarget target) =>
             new[] {
                 new DomainContributorsAssemblyBootstrapper()
             };
---
+++ tests/unit-tests/VolleyM.Domain.Contributors.UnitTests/IContributorsTestFixture.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using VolleyM.Domain.UnitTests.Framework;
+
+namespace VolleyM.Domain.Contributors.UnitTests
+{
+    public interface IContributorsTestFixture : ITestFixture
+    {
+        void MockSeveralContributorsExist(List<ContributorDto> testData);
+    }
+}

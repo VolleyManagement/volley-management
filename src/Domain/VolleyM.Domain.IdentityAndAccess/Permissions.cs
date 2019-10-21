309fc98341e2a1ad1c0b6be5a5bb010c6923b56a
Sergii Diachenko <sdiachenko@outlook.com>
2019-10-21 09:57:12 +0300

    Added specific permissions for IaA context

--- src/Domain/VolleyM.Domain.IdentityAndAccess/Permissions.cs
+++ src/Domain/VolleyM.Domain.IdentityAndAccess/Permissions.cs
@@ -1,16 +1,23 @@
-﻿using System.Reflection.Metadata;
-
+﻿// ReSharper disable InconsistentNaming
+#pragma warning disable IDE1006 // Naming Styles
 namespace VolleyM.Domain.IdentityAndAccess
 {
-    //ToDo: Use everywhere
     public static class Permissions
     {
         public static string Context { get; } = "IdentityAndAccess";
 
         public static class User
         {
-            public static string GetUser { get; } = "GetUser";
-            public static string CreateUser { get; } = "CreateUser";
+            public const string GetUser = "GetUser";
+            public const string CreateUser = "CreateUser";
+        }
+    }
+
+    internal class PermissionAttribute : Contracts.Crosscutting.PermissionAttribute
+    {
+        public PermissionAttribute(string action) : base(Permissions.Context, action)
+        {
         }
     }
 }
+#pragma warning restore IDE1006 // Naming Styles

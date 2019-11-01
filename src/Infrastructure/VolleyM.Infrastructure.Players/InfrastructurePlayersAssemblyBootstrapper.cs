db6a782aaa5f1dbdc076c5c8f95e1e5324840626
YarishPetro <YarishP17@gmail.com>
2019-11-01 17:19:26 +0200

    Added class InfrastructurePlayersAssemblyBootstrapper

---
+++ src/Infrastructure/VolleyM.Infrastructure.Players/InfrastructurePlayersAssemblyBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper.Configuration;
+using SimpleInjector;
+using System.Composition;
+using System.Reflection;
+using VolleyM.Domain.Contracts;
+
+using VolleyM.Infrastructure.Bootstrap;
+
+namespace VolleyM.Infrastructure.Players
+{
+    [Export(typeof(IAssemblyBootstrapper))]
+    class InfrastructurePlayersAssemblyBootstrapper : IAssemblyBootstrapper
+    {
+        public void RegisterDependencies(Container container, Microsoft.Extensions.Configuration.IConfiguration config)
+        {
+            container.Register(typeof(IQuery<,>), Assembly.GetAssembly(GetType()), Lifestyle.Scoped);
+        }
+        public void RegisterMappingProfiles(MapperConfigurationExpression mce)
+        {
+            // no mapping
+        }
+    }
+}

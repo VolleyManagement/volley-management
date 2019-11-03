cc7ac9c9b1c1075bcab398596f53fdc5188b385b
Sergii Diachenko <sdiachenko@outlook.com>
2019-11-03 08:14:54 +0200

    Fixed not nested handler test

--- src/Domain/VolleyM.Domain.Framework/HandlerMetadata/HandlerMetadataService.cs
+++ src/Domain/VolleyM.Domain.Framework/HandlerMetadata/HandlerMetadataService.cs
@@ -1,4 +1,7 @@
-﻿using LanguageExt;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using LanguageExt;
 using VolleyM.Domain.Contracts;
 
 namespace VolleyM.Domain.Framework.HandlerMetadata
@@ -8,10 +11,66 @@ namespace VolleyM.Domain.Framework.HandlerMetadata
     /// </summary>
     public class HandlerMetadataService
     {
-        public Either<Error, HandlerMetadata> GetHandlerMetadata<TRequest, TResponse>(IRequestHandler<TRequest, TResponse> handler) 
+        private readonly ConcurrentDictionary<Type, HandlerMetadata> _handlerMetadataCache
+            = new ConcurrentDictionary<Type, HandlerMetadata>();
+
+        public Either<Error, HandlerMetadata> GetHandlerMetadata<TRequest, TResponse>(IRequestHandler<TRequest, TResponse> handler)
             where TRequest : IRequest<TResponse>
         {
-            return Error.DesignViolation("Handler is allowed to implement only one IRequestHandler");
+            return from requestType in GetRequestType(handler)
+                   from metadata in GetOrCreateMetadata(handler, requestType)
+                   select metadata;
+        }
+
+        private static Either<Error, Type> GetRequestType<TRequest, TResponse>(IRequestHandler<TRequest, TResponse> handler)
+            where TRequest : IRequest<TResponse>
+        {
+            var handlerInterfaces = handler.GetType().GetInterfaces()
+                .Where(IsIRequestHandler<TRequest, TResponse>)
+                .ToArray();
+
+            if (handlerInterfaces.Length > 1)
+            {
+                return Error.DesignViolation("Handler is allowed to implement only one IRequestHandler");
+            }
+
+            return handlerInterfaces[0].GenericTypeArguments.First();
+        }
+
+        private Either<Error, HandlerMetadata> GetOrCreateMetadata(object handler, Type requestType)
+        {
+            Either<Error, HandlerMetadata> result;
+
+            if (!_handlerMetadataCache.TryGetValue(requestType, out var metadata))
+            {
+                result = GetHandlerMetadata(handler, requestType);
+
+                result.Match(
+                    Right: m => _handlerMetadataCache.AddOrUpdate(requestType, metadata, (t, m) => m), 
+                    Left: _ => { });
+            }
+            else
+            {
+                result = metadata;
+            }
+
+            return result;
+        }
+
+        private Either<Error, HandlerMetadata> GetHandlerMetadata(object handler, Type requestType)
+        {
+            var declaringType = handler.GetType().DeclaringType;
+
+            if (declaringType == null)
+                return Error.DesignViolation("Handler should be nested in a class to group handler related classes together");
+
+            return new HandlerMetadata();
+        }
+
+        private static bool IsIRequestHandler<TRequest, TResponse>(Type interfaceType) where TRequest : IRequest<TResponse>
+        {
+            var name = typeof(IRequestHandler<,>).Name;
+            return interfaceType.Name == name;
         }
     }
 }

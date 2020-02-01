496173a04552e3ca0868fd7e10fad4c1017cc64e
Sergii Diachenko <sdiachen@softserveinc.com>
2020-02-01 16:43:11 +0200

    SpyEventListener implemented

--- tests/unit-tests/VolleyM.Domain.Framework.UnitTests/Fixture/SpyEventListener.cs
+++ tests/unit-tests/VolleyM.Domain.Framework.UnitTests/Fixture/SpyEventListener.cs
@@ -8,9 +8,10 @@ namespace VolleyM.Domain.IDomainFrameworkTestFixture
     {
         public Task Handle(SampleEvent @event)
         {
-            throw new System.NotImplementedException();
+            Events.Add(@event);
+            return Task.CompletedTask;
         }
 
-        public List<object> Events { get; set; }
+        public List<object> Events { get; set; } = new List<object>();
     }
 }

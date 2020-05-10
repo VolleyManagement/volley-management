52b45f27d0a25e8aa8d7620c24848375fbed13c9
Sergii Diachenko <sdiachenko@outlook.com>
2020-05-10 19:01:52 +0300

    Fixed dependencies. Added config

---
+++ src/Client/VolleyM.API/VolleyMApiApplicationInfo.cs
@@ -0,0 +1,14 @@
+﻿using VolleyM.Domain.Contracts.Crosscutting;
+
+namespace VolleyM.API
+{
+	public class VolleyMApiApplicationInfo : IApplicationInfo
+	{
+		public VolleyMApiApplicationInfo(bool isRunningInProduction)
+		{
+			IsRunningInProduction = isRunningInProduction;
+		}
+
+		public bool IsRunningInProduction { get; }
+	}
+}

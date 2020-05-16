6cce7bc773b723477c0c4f6e0f29ab9619ef8637
Sergii Diachenko <sdiachen@users.noreply.github.com>
2020-05-16 20:24:36 +0300

    [AB#1130] Upgrade Karate to 0.9.6.RC2 ⬆ (#466)
    * Upgrade Karate to 0.9.6.RC2
    
    * Fixed universal artifact
    
    * Changed file encoding

--- tests/api-tests/get-karate.ps1
+++ tests/api-tests/get-karate.ps1
@@ -1,5 +1,5 @@
 # If you upgrade version, you need to update package in artifacts. See publish-karate.azcli
 
-$url = "https://dl.bintray.com/ptrthomas/karate/karate-0.9.4.jar";
+$url = "https://dl.bintray.com/ptrthomas/karate/karate-0.9.6.RC2.jar";
 
 Invoke-WebRequest -Uri $url -OutFile .\karate.jar -Headers @{'Host' = 'dl.bintray.com'; 'Referer' = 'https://dl.bintray.com/ptrthomas/karate/'} -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'

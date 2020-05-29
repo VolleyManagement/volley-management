4a3189510b76b2ed76a837c774fbad1bd1b29706
Sergii Diachenko <sdiachenko@outlook.com>
2020-05-29 22:05:11 +0300

    Added feature file

---
+++ tests/unit-tests/VolleyM.Domain.Players.UnitTests/Players/CorrectNameSteps.cs
@@ -0,0 +1,36 @@
+﻿namespace VolleyM.Domain.Players.UnitTests.Players
+{
+	public class CorrectNameSteps
+	{
+		[Given(@"player exists")]
+		public void GivenPlayerExists(Table table)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Given(@"I have CorrectNameRequest")]
+		public void GivenIHaveCorrectNameRequest(Table table)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I execute CorrectName")]
+		public void WhenIExecuteCorrectName()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"success result is returned")]
+		public void ThenSuccessResultIsReturned()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"player name is")]
+		public void ThenPlayerNameIs(Table table)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+	}
+}

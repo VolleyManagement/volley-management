2af9d9c19b3aeb4e8cba2e77b32752e9b4bf2006
Sergii Diachenko <sdiachenko@outlook.com>
2020-06-06 21:30:42 +0300

    Fixed formatting

--- tests/unit-tests/VolleyM.Domain.Players.UnitTests/EventAssertionStepsHook.cs
+++ tests/unit-tests/VolleyM.Domain.Players.UnitTests/EventAssertionStepsHook.cs
@@ -7,9 +7,9 @@ namespace VolleyM.Domain.Players.UnitTests
 	/// SpecFlow is not great at reusing steps across assemblies.
 	/// By having this class we make it think that it belongs to this assembly
 	/// </summary>
-	public class EventAssertionStepsHook:EventAssertionsSteps
+	public class EventAssertionStepsHook : EventAssertionsSteps
 	{
-		public EventAssertionStepsHook(Container container, ISpecFlowTransformFactory transformFactory) 
+		public EventAssertionStepsHook(Container container, ISpecFlowTransformFactory transformFactory)
 			: base(container, transformFactory)
 		{
 		}

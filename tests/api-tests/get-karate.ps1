404ae4d5aa40580ab2aedcd43445de30070c8811
Sergii Diachenko <sdiachenko@outlook.com>
2021-05-10 18:19:14 +0300

    docs: :chart_with_upwards_trend: Added context map

--- tests/api-tests/get-karate.ps1
+++ tests/api-tests/get-karate.ps1
@@ -1,5 +1,17 @@
-# If you upgrade version, you need to update package in artifacts. See publish-karate.azcli
+$karateVersion = 'karate-0.9.9.RC3'
 
-$url = "https://dl.bintray.com/ptrthomas/karate/karate-0.9.6.RC2.jar";
+$url = "https://dl.bintray.com/ptrthomas/karate/${karateVersion}.jar";
 
-Invoke-WebRequest -Uri $url -OutFile .\karate.jar -Headers @{'Host' = 'dl.bintray.com'; 'Referer' = 'https://dl.bintray.com/ptrthomas/karate/'} -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+$karateHome = '~\.karate'
+If (!(test-path $karateHome)) {
+	New-Item -ItemType Directory -Force -Path $karateHome
+}
+
+$karateFile = "${karateHome}\karate.jar";
+
+Invoke-WebRequest -Uri $url -OutFile $karateFile -Headers @{'Host' = 'dl.bintray.com'; 'Referer' = 'https://dl.bintray.com/ptrthomas/karate/' } -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+
+$karatePath = Resolve-Path -Path $karateFile
+
+$env:KARATE = $karatePath
+[System.Environment]::SetEnvironmentVariable('KARATE', $karatePath, [System.EnvironmentVariableTarget]::User)
